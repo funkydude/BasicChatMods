0ecd6f2cf6eca07126e97a03cd379e6c05ccded1
Vidar Haarr <vhaarr@gmail.com>
2007-02-14 14:39:24 +0000

    SimpleChatMods: antispam: Ignore some common spam site whispers, list ninjaed from spamsentry.

--- antispam.lua
+++ antispam.lua
@@ -2,6 +2,42 @@ local scmAntiSpam = {}
 
 SCM_ANTISPAM_HISTORY = 5
 
+local tokens = {
+	["29games"] = true,
+	["365ig"] = true,
+	["agamego"] = true,
+	["buyw0wgame"] = true,
+	["gamenoble"] = true,
+	["gmwork"] = true,
+	["gmw0rk"] = true,
+	["goldwithyou"] = true,
+	["hugold"] = true,
+	["igs36"] = true,
+	["igamebuy"] = true,
+	["itembay"] = true,
+	["mmoinn"] = true,
+	["ogchannel"] = true,
+	["ogmarket"] = true,
+	["gs365"] = true,
+	["ogs4u"] = true,
+	["okstar2008"] = true,
+	["peons4"] = true,
+	["player123"] = true,
+	["ssegames"] = true,
+	["speedpanda"] = true,
+	["wowcoming"] = true,
+	["woweurope.cn"] = true,
+	["wowforever"] = true,
+	["wowfreebuy"] = true,
+	["wowgoldsky"] = true,
+	["wowpanning"] = true,
+	["wowpfs"] = true,
+	["wowspa"] = true,
+	["wowstar2008"] = true,
+	["wowsupplier"] = true,
+	["zlywy"] = true,
+}
+
 local spam = {}
 local prehook = nil
 
@@ -16,6 +52,11 @@ function scmAntiSpam:Enable()
 			end
 			table.insert(spam, x)
 			spam[x] = 1
+
+			local spamString = arg1:gsub("%A", ""):trim():lower()
+			for k, v in pairs(tokens) do
+				if spamString:find(k) then return end
+			end
 		end
 
 		return prehook(...)
