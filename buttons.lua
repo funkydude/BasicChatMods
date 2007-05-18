d2c2dbc8ea08002b9a86cc005ab8847882ede423
Vidar Haarr <vhaarr@gmail.com>
2007-05-18 06:38:29 +0000

    SimpleChatMods: buttons: one function instead of 21.

--- buttons.lua
+++ buttons.lua
@@ -6,25 +6,27 @@ SMC_BUTTONS_SHOW_BOTTOM = nil
 
 local _G = getfenv(0)
 
+local function hide() this:Hide() end
+
 function scmButtons:Enable()
 	local a
 	ChatFrameMenuButton:Hide()
 	for i = 1, 7 do
 		if not SMC_BUTTONS_SHOW_UP then
 			a = _G["ChatFrame"..i.."UpButton"]
-			a:SetScript("OnShow", function() this:Hide() end)
+			a:SetScript("OnShow", hide)
 			a:Hide()
 		end
 
 		if not SMC_BUTTONS_SHOW_DOWN then
 			a = _G["ChatFrame"..i.."DownButton"]
-			a:SetScript("OnShow", function() this:Hide() end)
+			a:SetScript("OnShow", hide)
 			a:Hide()
 		end
 
 		if not SMC_BUTTONS_SHOW_BOTTOM then
 			a = _G["ChatFrame"..i.."BottomButton"]
-			a:SetScript("OnShow", function() this:Hide() end)
+			a:SetScript("OnShow", hide)
 			a:Hide()
 		end
 	end
