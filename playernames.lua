919f00645718e94cc81632d9ae5c7625e20a98bb
Vidar Haarr <vhaarr@gmail.com>
2007-06-21 08:35:19 +0000

    SimpleChatMods: playernames: Hopefully reduce memory usage slightly.

--- playernames.lua
+++ playernames.lua
@@ -16,14 +16,31 @@ SCM_PLAYERNAMES_COLORS = {
 	WARLOCK = "9382c9",
 	WARRIOR = "c69b6d"
 }
-scmPlayernames.Names = {}
 
-local _G = getfenv(0)
+local classes = {} -- Key: Name, Value: Class
+local names = setmetatable({}, {__index =
+	function(self, key) -- Key: Name
+		if classes[key] then
+			self[key] = string.format("|cff%s%s|r", SCM_PLAYERNAMES_COLORS[classes[key]], key)
+			return self[key]
+		end
+		return nil
+	end
+})
+
+local function addName(name, class)
+	if not class or not name or classes[name] then return end
+	class = class:upper()
+	if class == "UNKNOWN" or not SCM_PLAYERNAMES_COLORS[class] then return end
+	classes[name] = class
+end
 
 function scmPlayernames:OnEnable()
+	local _G = getfenv(0)
 	for i=1,NUM_CHAT_WINDOWS do
 		self:Hook(_G["ChatFrame"..i], "AddMessage", true)
 	end
+
 	self:RegisterBucketEvent("FRIENDLIST_UPDATE", 5, "updateFriends")
 	self:RegisterBucketEvent("GUILD_ROSTER_UPDATE", 5, "updateGuild")
 	self:RegisterBucketEvent("RAID_ROSTER_UPDATE", 5, "updateRaid")
@@ -31,12 +48,13 @@ function scmPlayernames:OnEnable()
 	self:RegisterBucketEvent("WHO_LIST_UPDATE", 5, "updateWho")
 	self:RegisterEvent("PLAYER_TARGET_CHANGED", "updateTarget")
 	self:RegisterEvent("CHAT_MSG_SYSTEM", "updateSystem")
+
 	if SCM_PLAYERNAMES_MOUSEOVER then
 		self:RegisterEvent("UPDATE_MOUSEOVER_UNIT", "updateMouseover")
 	end
 
 	local pclass = select(2, UnitClass("player"))
-	self:addName(UnitName("player"), pclass)
+	addName(UnitName("player"), pclass)
 
 	if IsInGuild() then
 		GuildRoster()
@@ -47,76 +65,60 @@ function scmPlayernames:OnEnable()
 end
 
 function scmPlayernames:updateFriends()
-	local Name, Class
 	for i = 1, GetNumFriends() do
-		Name, _, Class = GetFriendInfo(i)
-		self:addName(Name, Class)
+		local name, _, class = GetFriendInfo(i)
+		addName(name, class)
 	end
 end
 
 function scmPlayernames:updateGuild()
-	local Name, Class
 	for i = 1, GetNumGuildMembers() do
-		Name, _, _, _, Class = GetGuildRosterInfo(i)
-		self:addName(Name, Class)
+		local name, _, _, _, class = GetGuildRosterInfo(i)
+		addName(name, class)
 	end
 end
 
 function scmPlayernames:updateRaid()
-	local Name, Class
 	for i = 1, GetNumRaidMembers() do
-		Name, _, _, _, _, Class = GetRaidRosterInfo(i)
-		self:addName(Name, Class)
+		name, _, _, _, _, class = GetRaidRosterInfo(i)
+		addName(name, class)
 	end
 end
 
 function scmPlayernames:updateParty()
-	local Name, Class
 	for i = 1, GetNumPartyMembers() do
-		Name = UnitName("party" .. i)
-		Class = select(2, UnitClass("party" .. i))
-		self:addName(Name, Class)
+		addName(UnitName("party" .. i), select(2, UnitClass("party" .. i)))
 	end
 end
 
 function scmPlayernames:updateTarget()
 	if not UnitExists("target") or not UnitIsPlayer("target") or not UnitIsFriend("player", "target") then return end
-	local Class = select(2, UnitClass("target"))
-	self:addName(UnitName("target"), Class)
+	addName(UnitName("target"), select(2, UnitClass("target")))
 end
 
 function scmPlayernames:updateMouseover()
 	if not UnitExists("mouseover") or not UnitIsPlayer("mouseover") or not UnitIsFriend("player", "mouseover") then return end
-	local Class = select(2, UnitClass("mouseover"))
-	self:addName(UnitName("mouseover"), Class)
+	addName(UnitName("mouseover"), select(2, UnitClass("mouseover")))
 end
 
 function scmPlayernames:updateSystem( msg )
 	local name, class = select(3, msg:find("^|Hplayer:%w+|h%[(%w+)%]|h: Level %d+ %w+ (%w+) %- .+$"))
 	if name and class then 
-		self:addName(name, class) 
+		addName(name, class)
 	end
 end
 
 function scmPlayernames:updateWho()
-	local Name, Class
 	for i = 1, GetNumWhoResults() do
-		Name, _, _, _, Class = GetWhoInfo(i)
-		self:addName(Name, Class)
+		local name, _, _, _, class = GetWhoInfo(i)
+		addName(name, class)
 	end
-end
-
-function scmPlayernames:addName(Name, Class)
-	if not Class or not Name then return end
-	Class = Class:upper()
-	if Class == "UNKNOWN" or not SCM_PLAYERNAMES_COLORS[Class] then return end
-	self.Names[Name] = string.format("|cff%s%s|r", SCM_PLAYERNAMES_COLORS[Class], Name)
 end
 
 function scmPlayernames:AddMessage(frame, text, ...)
 	if type(text) == "string" then
 		local name = text:gsub(".*|Hplayer:(.-):?%d*|h.*", "%1")
-		if self.Names[name] then name = self.Names[name] end
+		if names[name] then name = names[name] end
 		text = text:gsub("|Hplayer:(.-)|h%[.-%]|h.-:", SCM_PLAYERNAMES_LEFTBRACKET.."|Hplayer:%1|h" .. name .. "|h"..SCM_PLAYERNAMES_RIGHTBRACKET)
 	end
 
