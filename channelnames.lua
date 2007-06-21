cfc29f476687015e9ac50f0294c5cb6329c6c64c
Vidar Haarr <vhaarr@gmail.com>
2007-06-21 09:38:20 +0000

    SimpleChatMods: channelnames: Fix the channel regexps.

--- channelnames.lua
+++ channelnames.lua
@@ -1,7 +1,5 @@
 scmChannelnames = AceLibrary("AceAddon-2.0"):new("AceHook-2.1")
 
-local _G = getfenv(0)
-
 SCM_CHANNELNAMES_REGEXPS = {
 	["%[Guild%]"] = "(G)",
 	["%[Party%]"] = "(P)",
@@ -10,15 +8,20 @@ SCM_CHANNELNAMES_REGEXPS = {
 	["%[Raid Warning%]"] = "|cffff0000(|rRW|cffff0000)|r",
 	["%[Officer%]"] = "(O)",
 	["%[%d+%. WorldDefense%]"] = "|cff990066(|rWD|cff990066)|r",
-	["%[%d+%. General.-%]"] = "|cff990066(|rGEN|cff990066)|r",
-	["%[%d+%. LocalDefense.-%]"] = "|cff990066(|rLD|cff990066)|r",
-	["%[%d+%. Trade.-%]"] = "|cff990066(|rT|cff990066)|r",
+	["%[%d+%. General%]"] = "|cff990066(|rGEN|cff990066)|r",
+	["%[%d+%. LocalDefense%]"] = "|cff990066(|rLD|cff990066)|r",
+	["%[%d+%. Trade%]"] = "|cff990066(|rT|cff990066)|r",
+	["%[%d+%. GuildRecruitment %- .*%]"] = "|cff990066(|rGR|cff990066)|r",
 	["%[Battleground%]"] = "|cffff3399(|rBG|cffff3399)|r",
 	["%[Battleground Leader%]"] = "|cffff0000(|rBGL|cffff0000)|r",
-	["%[%d+%.%s(.-)%]"] = "|cff990066(|r%1|cff990066)|r",
+}
+
+local customChannels = {
+	["%[%d+%.%s(.*)%]"] = "|cff990066(|r%1|cff990066)|r",
 }
 
 function scmChannelnames:OnEnable()
+	local _G = getfenv(0)
 	for i = 1, 7 do
 		self:Hook(_G["ChatFrame"..i], "AddMessage", true)
 	end
@@ -27,6 +30,9 @@ end
 function scmChannelnames:AddMessage(frame, text, r, g, b, id)
 	if type(text) == "string" then
 		for k, v in pairs(SCM_CHANNELNAMES_REGEXPS) do
+			text = text:gsub(k, v)
+		end
+		for k, v in pairs(customChannels) do
 			text = text:gsub(k, v)
 		end
 	end
