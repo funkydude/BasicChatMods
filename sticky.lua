0ab76ce3335f4b16de0322898fa8217c394b22c7
funkydude <funkydude@users.noreply.github.com>
2007-12-07 03:48:38 +0000

    BasicChatMods: make the sticky module actually work.

--- sticky.lua
+++ sticky.lua
@@ -1,32 +1,19 @@
-bcmSticky = {}
-local bcmSticky = bcmSticky
 
-local BCM_STICKY = {
-	["SAY"] = true,
-	["YELL"] = true,
-	["PARTY"] = true,
-	["GUILD"] = true,
-	["OFFICER"] = true,
-	["RAID"] = true,
-	["RAID_WARNING"] = true,
-	["BATTLEGROUND"] = true,
-	["CHANNEL"] = true,
-	["EMOTE"] = true
-}
+--[[		Sticky Channels		]]--
 
-function bcmSticky:Enable()
-	for k, v in pairs( BCM_STICKY ) do
-		if v then
-			ChatTypeInfo[k].sticky = 1
-		else
-			ChatTypeInfo[k].sticky = 0
-		end
-	end
-end
 
-function bcmSticky:Disable()
-	for k, v in pairs( BCM_STICKY ) do
-		ChatTypeInfo[k].sticky = 0
-	end
-end
+--[[		Following are on(1) by default, uncomment them to turn them off		]]--
+--ChatTypeInfo.SAY.sticky = 0
+--ChatTypeInfo.PARTY.sticky = 0
+--ChatTypeInfo.GUILD.sticky = 0
+--ChatTypeInfo.RAID.sticky = 0
+--ChatTypeInfo.BATTLEGROUND.sticky = 0
 
+
+--[[		Following are off(0) by default so we turn them on, comment them(--) to turn them off		]]--
+ChatTypeInfo.EMOTE.sticky = 1
+ChatTypeInfo.YELL.sticky = 1
+ChatTypeInfo.OFFICER.sticky = 1
+ChatTypeInfo.RAID_WARNING.sticky = 1
+ChatTypeInfo.WHISPER.sticky = 1
+ChatTypeInfo.CHANNEL.sticky = 1
