16577e782e7e7148feacafa39d927de3a9e1250c
funkydude <funkydude@users.noreply.github.com>
2008-07-04 18:17:58 +0000

    BasicChatMods: bugfix font module setting font size really small on a fresh install

--- font.lua
+++ font.lua
@@ -1,13 +1,14 @@
+if not _G.LibStub then return end
 
 --[[		Settings		]]--
 local FONT = "Arial Narrow"
 
 
 --[[		Chat Font Module		]]--
-local media = LibStub("LibSharedMedia-3.0", true)
+local media = _G.LibStub("LibSharedMedia-3.0", true)
 if not media then return end
 
-for i = 1, NUM_CHAT_WINDOWS do
-	_G[format("%s%d", "ChatFrame", i)]:SetFont(media:Fetch("font", FONT), 1)
+for i = 1, _G.NUM_CHAT_WINDOWS do
+	_G[format("%s%d", "ChatFrame", i)]:SetFont(media:Fetch("font", FONT), 12)
 end
 
