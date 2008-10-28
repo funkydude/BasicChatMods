4e0ca5fdae031f0549c766c66d0bc469fbb8d2b3
funkydude <funkydude@users.noreply.github.com>
2008-10-28 16:38:15 +0000

    BasicChatMods: Updated and fixed timestamps module (it was breaking the blizz combat log when blizz timestamps were on)

--- timestamps.lua
+++ timestamps.lua
@@ -1,14 +1,21 @@
 
---[[		Timestamp Color		]]--
+--[[		Settings		]]--
+--Timestamp coloring, http://www.december.com/html/spec/colorcodes.html
 local COLOR = "777777"
+--Formats decide what time format you want http://www.lua.org/pil/22.1.html
+--You can also mix in symbols like %I.%M or or %x:%X
+local tformat = "%X"
+--Left and right bracket, change to what you want, or make blank "" (Keep the |r)
+local lbrack, rbrack = "[", "]|r "
+
 
 --[[		Timestamp Module		]]--
 local date = _G.date
 local hooks = {}
 local h = nil
-
+local start = "|cff"
 local function AddMessage(frame, text, ...)
-	text = "|cff"..COLOR.."["..date("%X").."]|r "..text
+	text = start..COLOR..lbrack..date(tformat)..rbrack..text
 	return hooks[frame](frame, text, ...)
 end
 
@@ -26,4 +33,4 @@ end
 	You will need to turn timestamps on
 	youself in the combatlog settings
 ]]--
-TEXT_MODE_A_STRING_TIMESTAMP = "|cff"..COLOR.."[$time]|r"
+_G.TEXT_MODE_A_STRING_TIMESTAMP = "|cff"..COLOR.."[%s]|r %s"
