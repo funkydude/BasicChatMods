06d365e6b42d2d29d9eccaeeda136181afdbe7fe
funkydude <funkydude@users.noreply.github.com>
2009-12-18 19:56:39 +0000

    urlcopy: Add CHAT_MSG_PARTY_LEADER so you can copy URL's from party leaders.

--- urlcopy.lua
+++ urlcopy.lua
@@ -83,8 +83,8 @@ local function AddEvents()
 	local events = {
 		"CHAT_MSG_CHANNEL", "CHAT_MSG_YELL",
 		"CHAT_MSG_GUILD", "CHAT_MSG_OFFICER",
-		"CHAT_MSG_PARTY", "CHAT_MSG_RAID",
-		"CHAT_MSG_RAID_LEADER",
+		"CHAT_MSG_PARTY", "CHAT_MSG_PARTY_LEADER",
+		"CHAT_MSG_RAID", "CHAT_MSG_RAID_LEADER",
 		"CHAT_MSG_SAY", "CHAT_MSG_WHISPER",
 	}
 	for _,event in ipairs(events) do
