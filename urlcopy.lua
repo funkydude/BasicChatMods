5251d0dd19f42b1f7ba998897f5317e2b717cd02
funkydude <funkydude@users.noreply.github.com>
2009-12-19 16:35:02 +0000

    urlcopy: tweaks

--- urlcopy.lua
+++ urlcopy.lua
@@ -2,13 +2,10 @@
 --[[		DON'T MODIFY		]]--
 
 local currentLink = nil
-local ref = _G["SetItemRef"]
 
 local gsub = _G.string.gsub
 local ipairs = _G.ipairs
 local pairs = _G.pairs
-local fmt = _G.string.format
-local sub = _G.string.sub
 
 local tlds = {
 COM = true,
@@ -38,17 +35,17 @@ local function Link(link, ...)
 	if not link then
 		return ""
 	end
-	return RegisterMatch(fmt(style, link, link))
+	return RegisterMatch((style):format(link, link))
 end
 local function Link_TLD(link, tld, ...)
 	if not link or not tld then
 		return ""
 	end
 	if tlds[strupper(tld)] then
-        return RegisterMatch(fmt(style, link, link))
-    else
-        return RegisterMatch(link)
-    end
+		return RegisterMatch((style):format(link, link))
+	else
+		return RegisterMatch(link)
+	end
 end
 
 local patterns = {
@@ -79,7 +76,7 @@ local function filterFunc(self, event, msg, ...)
 	return false, msg, ...
 end
 
-local function AddEvents()
+do
 	local events = {
 		"CHAT_MSG_CHANNEL", "CHAT_MSG_YELL",
 		"CHAT_MSG_GUILD", "CHAT_MSG_OFFICER",
@@ -90,40 +87,41 @@ local function AddEvents()
 	for _,event in ipairs(events) do
 		ChatFrame_AddMessageEventFilter(event, filterFunc)
 	end
-end
-AddEvents()
+	wipe(events)
 
-local function SetItem(link, ...)
-	if sub(link, 1, 3) == "url" then
-		currentLink = sub(link, 5)
-		StaticPopup_Show("BCMUrlCopyDialog")
-		return
+	local ref = _G["SetItemRef"]
+	local function SetItem(link, ...)
+		if (link):sub(1, 3) == "url" then
+			currentLink = (link):sub(5)
+			StaticPopup_Show("BCMUrlCopyDialog")
+			return
+		end
+		return ref(link, ...)
 	end
-	return ref(link, ...)
-end
-_G["SetItemRef"] = SetItem
+	_G["SetItemRef"] = SetItem
 
---[[		Popup Box		]]--
-StaticPopupDialogs["BCMUrlCopyDialog"] = {
-	text = "URL",
-	button2 = TEXT(CLOSE),
-	hasEditBox = 1,
-	hasWideEditBox = 1,
-	showAlert = 1,
-	OnShow = function()
-		local editBox = _G[this:GetName().."WideEditBox"]
-		editBox:SetText(currentLink)
-		editBox:SetFocus()
-		editBox:HighlightText(0)
-		local button = _G[this:GetName().."Button2"]
-		button:ClearAllPoints()
-		button:SetWidth(200)
-		button:SetPoint("CENTER", editBox, "CENTER", 0, -30)
-		_G[this:GetName().."AlertIcon"]:Hide()
-	end,
-	EditBoxOnEscapePressed = function() this:GetParent():Hide() end,
-	timeout = 0,
-	whileDead = 1,
-	hideOnEscape = 1,
-}
+	--[[		Popup Box		]]--
+	StaticPopupDialogs["BCMUrlCopyDialog"] = {
+		text = "URL",
+		button2 = TEXT(CLOSE),
+		hasEditBox = 1,
+		hasWideEditBox = 1,
+		showAlert = 1,
+		OnShow = function(frame)
+			local editBox = _G[frame:GetName().."WideEditBox"]
+			editBox:SetText(currentLink)
+			editBox:SetFocus()
+			editBox:HighlightText(0)
+			local button = _G[frame:GetName().."Button2"]
+			button:ClearAllPoints()
+			button:SetWidth(200)
+			button:SetPoint("CENTER", editBox, "CENTER", 0, -30)
+			_G[frame:GetName().."AlertIcon"]:Hide()
+		end,
+		EditBoxOnEscapePressed = function(frame) frame:GetParent():Hide() end,
+		timeout = 0,
+		whileDead = 1,
+		hideOnEscape = 1,
+	}
+end
 
