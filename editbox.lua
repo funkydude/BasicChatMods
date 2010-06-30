1e2608556b67ea351deb92424312dca92b91954f
funkydude <funkydude@users.noreply.github.com>
2010-06-30 20:01:08 +0000

    Move editbox to the top of the chatframe again in 3.3.5

--- editbox.lua
+++ editbox.lua
@@ -5,24 +5,18 @@
 	Move the editbox to the top of the
 	chat frame instead of the bottom.
 
-	Change ChatFrame1 to ChatFramex
-	x being the number of the ChatFrame
-	you want to attach the box to.
-	Max chat frames is 7
+	Also enable arrow keyboard buttons
+	for editing text in the textbox.
 ]]--
 
 do
-	--Don't move in 3.3.5
-	local eb = ChatFrameEditBox
-	if eb then
+	for i =1, 10 do
+		local eb =  _G[format("%s%d%s", "ChatFrame", i, "EditBox")]
+		local cf = _G[format("%s%d", "ChatFrame", i)]
 		eb:ClearAllPoints()
-		eb:SetPoint("BOTTOMLEFT",  "ChatFrame1", "TOPLEFT",  -5, 0)
-		eb:SetPoint("BOTTOMRIGHT", "ChatFrame1", "TOPRIGHT", 5, 0)
+		eb:SetPoint("BOTTOMLEFT",  cf, "TOPLEFT",  -5, 0)
+		eb:SetPoint("BOTTOMRIGHT", cf, "TOPRIGHT", 5, 0)
 		eb:SetAltArrowKeyMode(false)
-	else
-		for i =1, 10 do
-			_G[format("%s%d%s", "ChatFrame", i, "EditBox")]:SetAltArrowKeyMode(false)
-		end
 	end
 end
 
