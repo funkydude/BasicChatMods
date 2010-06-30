f5e447ad8bd0676f30020f91bcf0637701fa993b
funkydude <funkydude@users.noreply.github.com>
2010-06-30 21:12:27 +0000

    Improve the chatcopy module and properly show the icon on-demand in 3.3.5

--- chatcopy.lua
+++ chatcopy.lua
@@ -6,16 +6,12 @@ local BUTTON_POSITION = "BOTTOMLEFT"
 --Try wowhead.com for spell icons
 local BUTTON_ICON = "Spell_ChargePositive"
 
-
 --[[		ChatCopy Module		]]--
 local lines = {}
-local frame = nil
-local editBox = nil
-local f = nil
 
 local function createFrames()
 	--[[		Create our frames on demand		]]--
-	frame = CreateFrame("Frame", "BCMCopyFrame", UIParent)
+	local frame = CreateFrame("Frame", "BCMCopyFrame", UIParent)
 	frame:SetBackdrop({bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
 		edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
 		tile = true, tileSize = 16, edgeSize = 16,
@@ -32,7 +28,7 @@ local function createFrames()
 	scrollArea:SetPoint("TOPLEFT", frame, "TOPLEFT", 8, -30)
 	scrollArea:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -30, 8)
 
-	editBox = CreateFrame("EditBox", "BCMCopyBox", frame)
+	local editBox = CreateFrame("EditBox", "BCMCopyBox", frame)
 	editBox:SetMultiLine(true)
 	editBox:SetMaxLetters(99999)
 	editBox:EnableMouse(true)
@@ -46,69 +42,72 @@ local function createFrames()
 
 	local close = CreateFrame("Button", "BCMCloseButton", frame, "UIPanelCloseButton")
 	close:SetPoint("TOPRIGHT", frame, "TOPRIGHT")
-
-	f = true
 end
 
-local function GetLines(...)
-	--[[		Grab all those lines		]]--
-	local ct = 1
-	for i = select("#", ...), 1, -1 do
-		local region = select(i, ...)
-		if region:GetObjectType() == "FontString" then
-			lines[ct] = tostring(region:GetText())
-			ct = ct + 1
-		end
-	end
-	return ct - 1
-end
-
-local function Copy(cf)
-	--[[		Stick all the grabbed text into our copying frame		]]--
-	local _, size = cf:GetFont()
-	FCF_SetChatWindowFontSize(cf, cf, 0.01)
-	local lineCt = GetLines(cf:GetRegions())
-	local text = table.concat(lines, "\n", 1, lineCt)
-	FCF_SetChatWindowFontSize(cf, cf, size)
-	if not f then createFrames() end
-	frame:Show()
-	editBox:SetText(text)
-	editBox:HighlightText(0)
-end
-
-for i = 1, 10 do
-	--[[		Create the magic button		]]--
-	local cf = _G[format("ChatFrame%d",  i)]
-	--Due to stacking ChatFrames, we have to make 7 buttons instead of 1 :/
-	--XXX Retest new method after 3.3.5
-	local button = CreateFrame("Button", format("BCMButtonCF%d", i), cf)
+do
+	local button = CreateFrame("Button", "BCMCopyChat")
 	button:SetPoint(BUTTON_POSITION)
 	button:SetHeight(10) --LoadUp Height
 	button:SetWidth(10) --LoadUp Width
 	button:SetNormalTexture("Interface\\Icons\\"..BUTTON_ICON)
 	button:SetHighlightTexture("Interface\\Buttons\\ButtonHilight-Square")
-	button:SetScript("OnClick", function() Copy(cf) button:Hide() end)
-	button:SetScript("OnEnter", function()
-		button:SetHeight(28) --Big Height
-		button:SetWidth(28) --Big Width
-		GameTooltip:SetOwner(button)
+	button:SetScript("OnClick", function(frame)
+		local cf = frame:GetParent()
+		local _, size = cf:GetFont()
+		FCF_SetChatWindowFontSize(cf, cf, 0.01)
+		local ct = 1
+		for i = select("#", cf:GetRegions()), 1, -1 do
+			local region = select(i, cf:GetRegions())
+			if region:GetObjectType() == "FontString" then
+				lines[ct] = tostring(region:GetText())
+				ct = ct + 1
+			end
+		end
+		local lineCt = ct - 1
+		local text = table.concat(lines, "\n", 1, lineCt)
+		FCF_SetChatWindowFontSize(cf, cf, size)
+		if not BCMCopyFrame then createFrames() end
+		BCMCopyFrame:Show()
+		BCMCopyBox:SetText(text)
+		BCMCopyBox:HighlightText(0)
+		wipe(lines)
+	end)
+	button:SetScript("OnEnter", function(frame)
+		frame:SetHeight(28) --Big Height
+		frame:SetWidth(28) --Big Width
+		GameTooltip:SetOwner(frame)
 		GameTooltip:ClearLines()
 		GameTooltip:AddLine("Click to copy text.")
 		GameTooltip:Show()
 	end)
-	button:SetScript("OnLeave", function()
-		button:SetHeight(10) --Small Height
-		button:SetWidth(10) --Small Width
+	button:SetScript("OnLeave", function(frame)
+		frame:SetHeight(10) --Small Height
+		frame:SetWidth(10) --Small Width
 		GameTooltip:Hide()
 	end)
 	button:Hide()
-	--[[		Show/Hide the button as needed		]]--
-	local tab = _G[format("ChatFrame%dTab", i)]
-	if ChatFrameEditBox then
-		tab:SetScript("OnShow", function() button:Show() end)
-		tab:SetScript("OnHide", function() button:Hide() end)
-	else
-		tab:SetScript("OnEnter", function() button:Show() end)
+
+	--[[		Show/Hide Copy Chat button on demand		]]--
+	local t = 0
+	local fadein = FCF_FadeInChatFrame
+	FCF_FadeInChatFrame = function(frame)
+		local time = GetTime()
+		if time - t > 0.5 then
+			--Stupid hack to make up for all docked chat frames being faded in
+			--e.g. if you have 5 frames on 1 dock, this will trigger 5 times.
+			BCMCopyChat:ClearAllPoints()
+			BCMCopyChat:SetParent(frame)
+			BCMCopyChat:SetPoint(BUTTON_POSITION, frame)
+			BCMCopyChat:Show()
+			t = time
+		end
+		return fadein(frame)
+	end
+
+	local fadeout = FCF_FadeOutChatFrame
+	FCF_FadeOutChatFrame = function(frame)
+		BCMCopyChat:Hide()
+		fadeout(frame)
 	end
 end
 
