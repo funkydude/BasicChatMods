67457178c8e35cbc7f9b8d3321fb80c8a3efeb3a
funkydude <funkydude@users.noreply.github.com>
2010-07-17 01:55:11 +0000

    Test out a re-written simplified urlcopy module.

--- urlcopy.lua
+++ urlcopy.lua
@@ -2,90 +2,46 @@
 --[[		DON'T MODIFY		]]--
 
 local currentLink = nil
-
 local gsub = _G.string.gsub
-local ipairs = _G.ipairs
 local pairs = _G.pairs
+local style = "|cffffffff|Hurl:%1|h[%1]|h|r"
 
 local tlds = {
-COM = true, UK = true, NET = true, INFO = true,
-CO = true, DE = true, FR = true, ES = true,
-BE = true, CC = true, US = true, KO = true,
-CH = true, TW = true, CN = true, RU = true,
-GR = true, IT = true, EU = true, TV = true,
-NL = true, IM = true, HU = true, ORG = true,
-}
-
-local style = "|cffffffff|Hurl:%s|h[%s]|h|r"
-local tokennum, matchTable = 1, {}
-local function RegisterMatch(text)
-	local token = "\255\254\253"..tokennum.."\253\254\255"
-	matchTable[token] = strreplace(text, "%", "%%")
-	tokennum = tokennum + 1
-	return token
-end
-local function Link(link, ...)
-	if not link then
-		return ""
-	end
-	return RegisterMatch((style):format(link, link))
-end
-local function Link_TLD(link, tld, ...)
-	if not link or not tld then
-		return ""
-	end
-	if tlds[strupper(tld)] then
-		return RegisterMatch((style):format(link, link))
-	else
-		return RegisterMatch(link)
-	end
-end
-
-local patterns = {
-		-- X://Y url
-	{ pattern = "^(%a[%w%.+-]+://%S+)", matchfunc=Link},
-	{ pattern = "%f[%S](%a[%w%.+-]+://%S+)", matchfunc=Link},
-		-- www.X.Y url
-	{ pattern = "^(www%.[%w_-%%]+%.%S+)", matchfunc=Link},
-	{ pattern = "%f[%S](www%.[%w_-%%]+%.%S+)", matchfunc=Link},
-		-- X@Y.Z email
-	{ pattern = "(%S+@[%w_-%%%.]+%.(%a%a+))", matchfunc=Link_TLD},
-		-- X.Y.Z/WWWWW url with path
-	{ pattern = "^([%w_-%%%.]+[%w_-%%]%.(%a%a+)/%S+)", matchfunc=Link_TLD},
-	{ pattern = "%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+)/%S+)", matchfunc=Link_TLD},
-		-- X.Y.Z url
-	{ pattern = "^([%w_-%%%.]+[%w_-%%]%.(%a%a+))", matchfunc=Link_TLD},
-	{ pattern = "%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+))", matchfunc=Link_TLD},
+	"[Cc][Oo][Mm]", "[Uu][Kk]", "[Nn][Ee][Tt]", "[Dd][Ee]", "[Ff][Rr]", "[Ee][Ss]",
+	"[Bb][Ee]", "[Cc][Cc]", "[Uu][Ss]", "[Kk][Oo]", "[Cc][Hh]", "[Tt][Ww]",
+	"[Cc][Nn]", "[Rr][Uu]", "[Gg][Rr]", "[Ii][Tt]", "[Ee][Uu]", "[Tt][Vv]",
+	"[Nn][Ll]", "[Hh][Uu]", "[Oo][Rr][Gg]"
 }
 
 local function filterFunc(self, event, msg, ...)
-	for _, v in ipairs(patterns) do
-		msg = gsub(msg, v.pattern, v.matchfunc)
+	for _,v in pairs(tlds) do
+		local newMsg, found = gsub(msg, "(%S-%."..v.."/?%S*)", style)
+		if found > 0 then
+			return false, newMsg, ...
+		end
 	end
-	for k,v in pairs(matchTable) do
-		msg = gsub(msg, k, v)
-		matchTable[k] = nil
+	local newMsg, found = gsub(msg, "(%d+%.%d+%.%d+%.%d+:?%d*/?%S*)", style)
+	if found > 0 then
+		return false, newMsg, ...
 	end
-	return false, msg, ...
 end
 
 do
-	local events = {
-		"CHAT_MSG_CHANNEL", "CHAT_MSG_YELL",
-		"CHAT_MSG_GUILD", "CHAT_MSG_OFFICER",
-		"CHAT_MSG_PARTY", "CHAT_MSG_PARTY_LEADER",
-		"CHAT_MSG_RAID", "CHAT_MSG_RAID_LEADER",
-		"CHAT_MSG_SAY", "CHAT_MSG_WHISPER",
-		"CHAT_MSG_BN_WHISPER", "CHAT_MSG_BN_CONVERSATION",
-	}
-	for _,event in ipairs(events) do
-		ChatFrame_AddMessageEventFilter(event, filterFunc)
-	end
-	wipe(events)
-	events = nil
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_GUILD", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_OFFICER", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY_LEADER", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_LEADER", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_BN_WHISPER", filterFunc)
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_BN_CONVERSATION", filterFunc)
 
-	local ref = _G["SetItemRef"]
-	local function SetItem(link, ...)
+	local ref = SetItemRef
+	SetItemRef = function(link, ...)
 		if (link):sub(1, 3) == "url" then
 			currentLink = (link):sub(5)
 			StaticPopup_Show("BCMUrlCopyDialog")
@@ -93,7 +49,6 @@ do
 		end
 		return ref(link, ...)
 	end
-	_G["SetItemRef"] = SetItem
 
 	--[[		Popup Box		]]--
 	StaticPopupDialogs["BCMUrlCopyDialog"] = {
@@ -105,6 +60,7 @@ do
 		OnShow = function(frame)
 			local editBox = _G[frame:GetName().."WideEditBox"]
 			editBox:SetText(currentLink)
+			currentLink = nil
 			editBox:SetFocus()
 			editBox:HighlightText(0)
 			local button = _G[frame:GetName().."Button2"]
