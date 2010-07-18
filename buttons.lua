7320340a7e7138dbc4cbf4d59d93a9d22276147b
funkydude <funkydude@users.noreply.github.com>
2010-07-18 00:05:05 +0000

    tweak buttons module

--- buttons.lua
+++ buttons.lua
@@ -4,21 +4,30 @@
 --[[
 	If you want to show certain buttons, add "--" in
 	front of the line of code to disable hiding it
-	e.g. if you want to show the up/down arrows you'd change
-	--f.Show = f.Hide --Hide the up/down arrows
+
+	e.g. if you want to show the up/down arrows you'd change:
+
+	f:SetScript("OnShow", hideFunc) --Hide the up/down arrows
+	f:Hide() --Hide the up/down arrows
+
+	To:
+	--f:SetScript("OnShow", hideFunc) --Hide the up/down arrows
 	--f:Hide() --Hide the up/down arrows
+
 	So now -- has been added at the start, the line is disabled
 ]]--
 
 do
-	ChatFrameMenuButton.Show = ChatFrameMenuButton.Hide --Hide the chat shortcut button for emotes/languages/etc
+	local hideFunc = function(frame) frame:Hide() end
+
+	ChatFrameMenuButton:SetScript("OnShow", hideFunc) --Hide the chat shortcut button for emotes/languages/etc
 	ChatFrameMenuButton:Hide() --Hide the chat shortcut button for emotes/languages/etc
-	FriendsMicroButton.Show = FriendsMicroButton.Hide --Hide the "Friends Online" count button
+	FriendsMicroButton:SetScript("OnShow", hideFunc) --Hide the "Friends Online" count button
 	FriendsMicroButton:Hide() --Hide the "Friends Online" count button
 
 	for i = 1, 10 do
 		local f = _G[format("%s%d%s", "ChatFrame", i, "ButtonFrame")]
-		f.Show = f.Hide --Hide the up/down arrows
+		f:SetScript("OnShow", hideFunc) --Hide the up/down arrows
 		f:Hide() --Hide the up/down arrows
 	end
 end
