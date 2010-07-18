bbcb73dddb6731d31a9b006dd495bef07e0f5c72
funkydude <funkydude@users.noreply.github.com>
2010-07-18 01:51:37 +0000

    ChatCopy: Re-wrote module again, no longer uses a button to copy chat since it required hooking fadein/out and a hack, double click the chat tab to copy chat from the chat frame it belongs to.

--- chatcopy.lua
+++ chatcopy.lua
@@ -1,16 +1,18 @@
 
---[[		Settings		]]--
-
---TOP, BOTTOM, LEFT, RIGHT, BOTTOMLEFT, BOTTOMRIGHT, TOPLEFT, TOPRIGHT
-local BUTTON_POSITION = "BOTTOMLEFT"
---Try wowhead.com for spell icons
-local BUTTON_ICON = "Spell_ChargePositive"
-
 --[[		ChatCopy Module		]]--
-local lines = {}
 
-local function createFrames()
-	--[[		Create our frames on demand		]]--
+--[[
+	DO NOT MODIFY
+	This module allows you to easily
+	copy text from chat by allowing
+	you to double-click a chat frame tab
+	then displaying a pop-up containing
+	the entire chat frame history.
+]]--
+
+local lines = {}
+do
+	--Create Frames/Objects
 	local frame = CreateFrame("Frame", "BCMCopyFrame", UIParent)
 	frame:SetBackdrop({bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
 		edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
@@ -36,23 +38,16 @@ local function createFrames()
 	editBox:SetFontObject(ChatFontNormal)
 	editBox:SetWidth(400)
 	editBox:SetHeight(270)
-	editBox:SetScript("OnEscapePressed", function() frame:Hide() end)
+	editBox:SetScript("OnEscapePressed", function(f) f:GetParent():GetParent():Hide() f:SetText("") end)
 
 	scrollArea:SetScrollChild(editBox)
 
 	local close = CreateFrame("Button", "BCMCloseButton", frame, "UIPanelCloseButton")
 	close:SetPoint("TOPRIGHT", frame, "TOPRIGHT")
-end
 
-do
-	local button = CreateFrame("Button", "BCMCopyChat")
-	button:SetPoint(BUTTON_POSITION)
-	button:SetHeight(10) --LoadUp Height
-	button:SetWidth(10) --LoadUp Width
-	button:SetNormalTexture("Interface\\Icons\\"..BUTTON_ICON)
-	button:SetHighlightTexture("Interface\\Buttons\\ButtonHilight-Square")
-	button:SetScript("OnClick", function(frame)
-		local cf = frame:GetParent()
+	--Copying Functions
+	local copyFunc = function(frame, btn)
+		local cf = _G[strreplace(frame:GetName(), "Tab", "")]
 		local _, size = cf:GetFont()
 		FCF_SetChatWindowFontSize(cf, cf, 0.01)
 		local ct = 1
@@ -66,48 +61,14 @@ do
 		local lineCt = ct - 1
 		local text = table.concat(lines, "\n", 1, lineCt)
 		FCF_SetChatWindowFontSize(cf, cf, size)
-		if not BCMCopyFrame then createFrames() end
 		BCMCopyFrame:Show()
 		BCMCopyBox:SetText(text)
 		BCMCopyBox:HighlightText(0)
 		wipe(lines)
-	end)
-	button:SetScript("OnEnter", function(frame)
-		frame:SetHeight(28) --Big Height
-		frame:SetWidth(28) --Big Width
-		GameTooltip:SetOwner(frame)
-		GameTooltip:ClearLines()
-		GameTooltip:AddLine("Click to copy text.")
-		GameTooltip:Show()
-	end)
-	button:SetScript("OnLeave", function(frame)
-		frame:SetHeight(10) --Small Height
-		frame:SetWidth(10) --Small Width
-		GameTooltip:Hide()
-	end)
-	button:Hide()
-
-	--[[		Show/Hide Copy Chat button on demand		]]--
-	local t = 0
-	local fadein = FCF_FadeInChatFrame
-	FCF_FadeInChatFrame = function(frame)
-		local time = GetTime()
-		if time - t > 0.5 then
-			--Stupid hack to make up for all docked chat frames being faded in
-			--e.g. if you have 5 frames on 1 dock, this will trigger 5 times.
-			BCMCopyChat:ClearAllPoints()
-			BCMCopyChat:SetParent(frame)
-			BCMCopyChat:SetPoint(BUTTON_POSITION, frame)
-			BCMCopyChat:Show()
-			t = time
-		end
-		return fadein(frame)
 	end
-
-	local fadeout = FCF_FadeOutChatFrame
-	FCF_FadeOutChatFrame = function(frame)
-		BCMCopyChat:Hide()
-		fadeout(frame)
+	for i = 1, 10 do
+		local tab = _G[format("%s%d%s", "ChatFrame", i, "Tab")]
+		tab:SetScript("OnDoubleClick", copyFunc)
 	end
 end
 
