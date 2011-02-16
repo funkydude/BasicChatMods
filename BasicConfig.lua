f0d069ff8d014cbf487b9373149c89ab9ce342f7
funkydude <funkydude@users.noreply.github.com>
2011-02-16 13:10:53 +0000

    BasicConfig: tweaks

--- BasicConfig.lua
+++ BasicConfig.lua
@@ -1,54 +1,52 @@
 
 local name, f = ...
-local onClick = function(frame)
-	local tick = frame:GetChecked()
-	if tick then
-		PlaySound("igMainMenuOptionCheckBoxOn")
-		bcmDB[frame:GetParent():GetName()] = nil
-	else
-		PlaySound("igMainMenuOptionCheckBoxOff")
-		bcmDB[frame:GetParent():GetName()] = true
-	end
-end
-local onShow = function(frame)
-	if bcmDB[frame:GetParent():GetName()] then
-		frame:SetChecked(false)
-	else
-		frame:SetChecked(true)
-	end
-end
-local makeButton = function(parent)
-	--GlueXML/OptionsPanelTemplates.xml --> OptionsBaseCheckButtonTemplate
-	local frame = CreateFrame("CheckButton", nil, parent)
-	frame:SetWidth(26)
-	frame:SetHeight(26)
-	frame:SetHitRectInsets(0, -100, 0, 0)
-	frame:SetNormalTexture("Interface\\Buttons\\UI-CheckBox-Up")
-	frame:SetPushedTexture("Interface\\Buttons\\UI-CheckBox-Down")
-	frame:SetHighlightTexture("Interface\\Buttons\\UI-CheckBox-Highlight")
-	frame:SetCheckedTexture("Interface\\Buttons\\UI-CheckBox-Check")
-	frame:SetScript("OnClick", onClick)
-	frame:SetScript("OnShow", onShow)
-	frame:SetPoint("TOPLEFT", 16, -80)
-	local frameText = parent:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-	frameText:SetPoint("LEFT", frame, "RIGHT", 0, 1)
-	frameText:SetText(ENABLE)
-end
-local makePanel = function(frameName, bcm, panelName, desc)
-	local panel = CreateFrame("Frame", frameName, bcm)
-	panel.name, panel.parent = panelName, name
-	local panelDesc = panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
-	panelDesc:SetPoint("TOPLEFT", 16, -20)
-	panelDesc:SetText(desc)
-	panelDesc:SetWidth(350)
-	panelDesc:SetWordWrap(true)
-	InterfaceOptions_AddCategory(panel)
-	makeButton(panel)
-end
 f.functions[#f.functions+1] = function()
 	if bcmDB.noconfig then
-		makeButton, onShow, onClick = nil, nil, nil
 		return
+	end
+
+	local onClick = function(frame)
+		local tick = frame:GetChecked()
+		if tick then
+			PlaySound("igMainMenuOptionCheckBoxOn")
+			bcmDB[frame:GetParent():GetName()] = nil
+		else
+			PlaySound("igMainMenuOptionCheckBoxOff")
+			bcmDB[frame:GetParent():GetName()] = true
+		end
+	end
+	local onShow = function(frame)
+		if bcmDB[frame:GetParent():GetName()] then
+			frame:SetChecked(false)
+		else
+			frame:SetChecked(true)
+		end
+	end
+	local makePanel = function(frameName, bcm, panelName, desc)
+		local panel = CreateFrame("Frame", frameName, bcm)
+		panel.name, panel.parent = panelName, name
+		local panelDesc = panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		panelDesc:SetPoint("TOPLEFT", 16, -20)
+		panelDesc:SetText(desc)
+		panelDesc:SetWidth(350)
+		panelDesc:SetWordWrap(true)
+		InterfaceOptions_AddCategory(panel)
+
+		--GlueXML/OptionsPanelTemplates.xml --> OptionsBaseCheckButtonTemplate
+		local button = CreateFrame("CheckButton", nil, panel)
+		button:SetWidth(26)
+		button:SetHeight(26)
+		button:SetHitRectInsets(0, -100, 0, 0)
+		button:SetNormalTexture("Interface\\Buttons\\UI-CheckBox-Up")
+		button:SetPushedTexture("Interface\\Buttons\\UI-CheckBox-Down")
+		button:SetHighlightTexture("Interface\\Buttons\\UI-CheckBox-Highlight")
+		button:SetCheckedTexture("Interface\\Buttons\\UI-CheckBox-Check")
+		button:SetScript("OnClick", onClick)
+		button:SetScript("OnShow", onShow)
+		button:SetPoint("TOPLEFT", 16, -80)
+		local buttonText = panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+		buttonText:SetPoint("LEFT", button, "RIGHT", 0, 1)
+		buttonText:SetText(ENABLE)
 	end
 
 	--[[ Slash handler ]]--
@@ -90,9 +88,8 @@ f.functions[#f.functions+1] = function()
 	end
 
 	--[[ URLCopy Module ]]--
-	makePanel("BCM_URLCopy", bcm, "URL Copy", "This module turns websites in your chat frame into clickable links for you to copy. E.g. |cFFFFFFFF[www.facebook.com]|r")
+	makePanel("BCM_URLCopy", bcm, "URL Copy", "This module turns websites in your chat frame into clickable links for you to copy. E.g. |cFFFFFFFF[www.battle.net]|r")
 
-	makeButton = nil
 	makePanel = nil
 end
 
