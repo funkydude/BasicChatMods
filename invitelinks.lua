a17735955e049d5263cb4436ee338e51010d4666
funkydude <funkydude@users.noreply.github.com>
2011-02-21 04:44:29 +0000

    finished simple invite links module

--- invitelinks.lua
+++ invitelinks.lua
@@ -4,24 +4,12 @@
 local _, f = ...
 f.functions[#f.functions+1] = function()
 	if bcmDB.BCM_InviteLinks then return end
---[[
-	local tlds = {
-		"[Cc][Oo][Mm]", "[Uu][Kk]", "[Nn][Ee][Tt]", "[Dd][Ee]", "[Ff][Rr]", "[Ee][Ss]",
-		"[Bb][Ee]", "[Cc][Cc]", "[Uu][Ss]", "[Kk][Oo]", "[Cc][Hh]", "[Tt][Ww]",
-		"[Cc][Nn]", "[Rr][Uu]", "[Gg][Rr]", "[Ii][Tt]", "[Ee][Uu]", "[Tt][Vv]",
-		"[Nn][Ll]", "[Hh][Uu]", "[Oo][Rr][Gg]", "[Ss][Ee]", "[Nn][Oo]", "[Ff][Ii]"
-	}
-	local gsub = gsub
-	local filterFunc = function(self, event, msg, ...)
-		for i=1, 24 do --Number of TLD's in tlds table
-			local newMsg, found = gsub(msg, "(%S-%."..tlds[i].."/?%S*)", "|cffffffff|Hurl:%1|h[%1]|h|r")
-			if found > 0 then
-				return false, newMsg, ...
-			end
-		end
-		local newMsg, found = gsub(msg, "(%d+%.%d+%.%d+%.%d+:?%d*/?%S*)", "|cffffffff|Hurl:%1|h[%1]|h|r")
+
+	local trigger = "[Ii][Nn][Vv][Ii][Tt][Ee]"
+	local filterFunc = function(self, event, msg, player, ...)
+		local newMsg, found = gsub(msg, trigger, "|cffFF7256|Hinvite:"..player.."|h[%1]|h|r")
 		if found > 0 then
-			return false, newMsg, ...
+			return false, newMsg, player, ...
 		end
 	end
 
@@ -30,16 +18,15 @@ f.functions[#f.functions+1] = function()
 	ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", filterFunc)
 	ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", filterFunc)
 
-	local currentLink = nil
 	local oldShow = ChatFrame_OnHyperlinkShow
 	ChatFrame_OnHyperlinkShow = function(self, link, ...)
-		if (link):sub(1, 3) == "url" then
-			currentLink = (link):sub(5)
-			StaticPopup_Show("BCM_URLCopyBox")
+		if (link):sub(1, 6) == "invite" then
+			if IsAltKeyDown() then
+				InviteUnit((link):sub(8))
+			end
 			return
 		end
 		oldShow(self, link, ...)
 	end
-]]
 end
 
