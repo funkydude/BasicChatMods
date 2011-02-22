ec08a6c6b37f5df8f27ddc1b72a2efea8748df17
funkydude <funkydude@users.noreply.github.com>
2011-02-22 15:23:45 +0000

    editbox: Change to classic mode when using this module

--- editbox.lua
+++ editbox.lua
@@ -5,12 +5,16 @@ local _, f = ...
 f.functions[#f.functions+1] = function()
 	if bcmDB.BCM_EditBox then return end
 
+	--Classic mode hides the editbox when not in use, IM mode fades it out
+	--since we move the editbox above the chat tabs, we don't want it always showing
+	SetCVar("chatStyle", "classic")
 	for i =1, 10 do
 		local eb =  _G[format("%s%d%s", "ChatFrame", i, "EditBox")]
 		local cf = _G[format("%s%d", "ChatFrame", i)]
 		eb:ClearAllPoints()
 		eb:SetPoint("BOTTOMLEFT",  cf, "TOPLEFT",  -5, 0)
 		eb:SetPoint("BOTTOMRIGHT", cf, "TOPRIGHT", 5, 0)
+		eb:Hide() --call this incase we're just changing to classic mode for the first time
 	end
 end
 
