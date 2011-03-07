cec06d2f9e2cbfdaf797601393c39b42e310f80a
funkydude <funkydude@users.noreply.github.com>
2011-03-07 03:35:26 +0000

    playernames: add ability to class color RealID whispers, thanks to Stanzilla for being a late night test dummy.

--- playernames.lua
+++ playernames.lua
@@ -4,7 +4,7 @@
 local _, f = ...
 f.functions[#f.functions+1] = function()
 	local bcmDB = bcmDB
-	if bcmDB.BCM_PlayerNames then bcmDB.nolevel = nil bcmDB.nogroup = nil return end
+	if bcmDB.BCM_PlayerNames then bcmDB.nolevel = nil bcmDB.nogroup = nil bcmDB.nobnet = nil return end
 
 	local newAddMsg, nameLevels, nameGroup, frame = {}, {}, {}, CreateFrame("Frame")
 
@@ -66,9 +66,30 @@ f.functions[#f.functions+1] = function()
 		end
 		return ("|Hplayer:%s:%s[%s]|h"):format(name, misc, nameToChange)
 	end
+	local changeBnetName = function(misc, id, moreMisc, fakeName)
+		local englishClass = select(7, BNGetToonInfo(id))
+		local class
+		for k,v in pairs(LOCALIZED_CLASS_NAMES_FEMALE) do
+			if v == englishClass then class = k break end
+		end
+		if not class then
+			for k,v in pairs(LOCALIZED_CLASS_NAMES_MALE) do
+				if v == englishClass then class = k break end
+			end
+		end
+		local tbl = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class]
+		local color = ("|cFF%02x%02x%02x%s|r"):format(tbl.r*255, tbl.g*255, tbl.b*255, fakeName)
+		return ("|HBNplayer:%s|k:%s:%s|h[%s]|h"):format(misc, id, moreMisc, color)
+	end
 	local AddMessage = function(frame, text, ...)
-		text = text:gsub("|Hplayer:(.-):(.+)%[(.-)%]|h", changeName)
+		if not bcmDB.nolevel or not bcmDB.nogroup then
+			text = text:gsub("|Hplayer:(.-):(.+)%[(.-)%]|h", changeName)
+		end
+		if not bcmDB.nobnet then
+			text = text:gsub("|HBNplayer:(.+)|k:(%d-):(.+)|h%[(.-)%]|h", changeBnetName)
+		end
 		--|Hplayer:Name:185:WHISPER:NAME|h[|cffc69b6dName|r]|h whispers: test 5
+		--|HBNplayer:|Kf17|k0000000000000000|k:17:93:BN_WHISPER:|Kf17|k0000000000000000|k|h[|Kf17|k0000000000000000|k]|h whispers: hi
 
 		return newAddMsg[frame:GetName()](frame, text, ...)
 	end
