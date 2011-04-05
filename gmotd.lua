53f54e4236fd8aa1d4a6683d51c76281cd66c255
funkydude <funkydude@users.noreply.github.com>
2011-04-05 14:05:33 +0000

    gmotd: add some comments

--- gmotd.lua
+++ gmotd.lua
@@ -3,15 +3,19 @@
 
 local _, BCM = ...
 BCM.modules[#BCM.modules+1] = function()
-	if not IsInGuild() or bcmDB.BCM_GMOTD then return end
+	if bcmDB.BCM_GMOTD or not IsInGuild() then return end
 
 	BCM.Events.t = GetTime()
-	BCM.Events.count = 0
+	BCM.Events.count = 1
 	BCM.Events:SetScript("OnUpdate", function(frame)
 		if (GetTime() - frame.t) > 10 then
 			local gmotd = GetGuildRosterMOTD()
-			if gmotd == "" then
-				if frame.count > 2 then
+			--Sometimes the GMOTD isn't available at login/reload due to lag.
+			--We delay printing until it is available, but we can't assume that
+			--no GMOTD means it isn't available, it might mean no GMOTD was set.
+			--If it's still blank after 5 attempts, there probably isn't one set.
+			if gmotd == "" or gmotd:find("^ +$") then
+				if frame.count > 4 then
 					frame.t = nil
 					frame.count = nil
 					frame:SetScript("OnUpdate", nil)
@@ -21,7 +25,7 @@ BCM.modules[#BCM.modules+1] = function()
 				end
 			else
 				local info = ChatTypeInfo.GUILD
-				ChatFrame1:AddMessage("|cFF33FF99BasicChatMods|r: "..(GUILD_MOTD_TEMPLATE):format(GetGuildRosterMOTD()), info.r, info.g, info.b)
+				ChatFrame1:AddMessage("|cFF33FF99BasicChatMods|r: "..(GUILD_MOTD_TEMPLATE):format(gmotd), info.r, info.g, info.b)
 				frame.t = nil
 				frame.count = nil
 				frame:SetScript("OnUpdate", nil)
