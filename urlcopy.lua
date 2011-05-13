eb7e28a9566c4f05023f6cec43bdeb163fb24f52
funkydude <funkydude@users.noreply.github.com>
2011-05-13 18:33:02 +0000

    urlcopy: attempt at improving pattern matching

--- urlcopy.lua
+++ urlcopy.lua
@@ -5,24 +5,46 @@ local _, BCM = ...
 BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_URLCopy then return end
 
-	local tlds = {
-		"[Cc][Oo][Mm]", "[Uu][Kk]", "[Nn][Ee][Tt]", "[Dd][Ee]", "[Ff][Rr]", "[Ee][Ss]",
-		"[Bb][Ee]", "[Cc][Cc]", "[Uu][Ss]", "[Kk][Oo]", "[Cc][Hh]", "[Tt][Ww]",
-		"[Cc][Nn]", "[Rr][Uu]", "[Gg][Rr]", "[Ii][Tt]", "[Ee][Uu]", "[Tt][Vv]",
-		"[Nn][Ll]", "[Hh][Uu]", "[Oo][Rr][Gg]", "[Ss][Ee]", "[Nn][Oo]", "[Ff][Ii]"
-	}
 	local gsub = gsub
 	local filterFunc = function(self, event, msg, ...)
-		for i=1, 24 do --Number of TLD's in tlds table
-			local newMsg, found = gsub(msg, "(%S-%."..tlds[i].."/?%S*)", "|cffffffff|Hurl:%1|h[%1]|h|r")
-			if found > 0 then
-				return false, newMsg, ...
-			end
-		end
-		local newMsg, found = gsub(msg, "(%d+%.%d+%.%d+%.%d+:?%d*/?%S*)", "|cffffffff|Hurl:%1|h[%1]|h|r")
-		if found > 0 then
-			return false, newMsg, ...
-		end
+		local newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?/%S+", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.[%a%-%d]+%.%a%a%a?/%S+", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?/", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.[%a%-%d]+%.%a%a%a?/", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?$", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.[%a%-%d]+%.%a%a%a?$", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "(http://[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?) ", "|cffffffff|Hurl:%1|h[%1]|h|r ")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "(http://[%a%-%d]*%.?[%a%-%d]+%.[%a%-%d]+%.%a%a%a?) ", "|cffffffff|Hurl:%1|h[%1]|h|r ")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?/%S+", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?/", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?$", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "([%a%-%d]*%.?[%a%-%d]+%.%a%a%a?) ", "|cffffffff|Hurl:%1|h[%1]|h|r ")
+		if found > 0 then return false, newMsg, ... end
+
+		newMsg, found = gsub(msg, "%d+%.%d+%.%d+%.%d+:?%d*/?%S*", "|cffffffff|Hurl:%1|h[%1]|h|r")
+		if found > 0 then return false, newMsg, ... end
 	end
 
 	ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filterFunc)
