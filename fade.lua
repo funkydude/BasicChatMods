441274a3e2a2537f418fbb825c6653731417803a
funkydude <funkydude@users.noreply.github.com>
2012-11-09 16:01:03 +0000

    Fade: Swap to using clean securehooks on the fade functions instead of just modifying the global fade variables, which were then passed to UIFade funcs, passing us into taint hell. The only global variable modified now is the fade-start delay which isn't passed anywhere, it's only ever used in the OnUpdate value comparison.

--- fade.lua
+++ fade.lua
@@ -5,13 +5,46 @@ local _, BCM = ...
 BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_Fade then return end
 
-	CHAT_FRAME_TAB_NORMAL_NOMOUSE_ALPHA = 0
-	CHAT_FRAME_TAB_SELECTED_NOMOUSE_ALPHA = 0
-	CHAT_FRAME_BUTTON_FRAME_MIN_ALPHA = 0
-	for i=1, BCM.chatFrames do
-		local tab = _G[format("%s%d%s", "ChatFrame", i, "Tab")]
-		tab:SetAlpha(0)
-		tab.noMouseAlpha = 0
-	end
+	-- Instead of just changing the global fading variables like we previously did,
+	-- we now cleanly securehook the fade out functions and do the fading ourself. This should prevent
+	-- any taints involved in passing those previously modified global variables to the UIFade functions,
+	-- which have been known to cause taint issues in the past.
+	-- This code is basically a replica of the Blizzard code which kills fading right after it's started by Blizz.
+	-- The only global variable we modify now is CHAT_TAB_HIDE_DELAY as it's the only way to kill delayed
+	-- fadeout of the chatframes, but this should be ok as it's only ever used in a non-secure OnUpdate handler
+	-- and is never passed elsewhere.
+	hooksecurefunc("FCF_FadeOutChatFrame", function(chatFrame)
+		local frameName = chatFrame:GetName()
+
+		for index, value in pairs(CHAT_FRAME_TEXTURES) do
+			local object = _G[frameName..value]
+			if object:IsShown() then
+				UIFrameFadeRemoveFrame(object)
+				object:SetAlpha(0)
+			end
+		end
+		if chatFrame == FCFDock_GetSelectedWindow(GENERAL_CHAT_DOCK) then
+			if GENERAL_CHAT_DOCK.overflowButton:IsShown() then
+				UIFrameFadeRemoveFrame(GENERAL_CHAT_DOCK.overflowButton)
+				GENERAL_CHAT_DOCK.overflowButton:SetAlpha(0)
+			end
+		end
+
+		local chatTab = _G[frameName.."Tab"]
+		UIFrameFadeRemoveFrame(chatTab)
+		chatTab:SetAlpha(0)
+
+		if not chatFrame.isDocked then
+			UIFrameFadeRemoveFrame(chatFrame.buttonFrame)
+			chatFrame.buttonFrame:SetAlpha(0)
+		end
+	end)
+	hooksecurefunc("FCFTab_UpdateAlpha", function(chatFrame)
+		local chatTab = _G[chatFrame:GetName().."Tab"]
+		if not chatFrame.hasBeenFaded then
+			chatTab:SetAlpha(0)
+		end
+	end)
+	CHAT_TAB_HIDE_DELAY = 0
 end
 
