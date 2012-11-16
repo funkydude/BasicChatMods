ba67786ed19d352f20eaa3280406bec32513c39e
funkydude <funkydude@users.noreply.github.com>
2012-11-16 03:09:28 +0000

    scrolldown: do this a different way

--- scrolldown.lua
+++ scrolldown.lua
@@ -5,26 +5,11 @@ local _, BCM = ...
 BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_ScrollDown or bcmDB.BCM_ButtonHide or not GetCVarBool("chatMouseScroll") then return end
 
-	local scrollFunc = function(frame, d)
-		if d then
-			if d > 0 then
-				if IsShiftKeyDown() then
-					frame:ScrollToTop()
-				elseif IsControlKeyDown() then
-					frame:PageUp()
-				--else -- Blizz function does this
-				--	frame:ScrollUp()
-				end
-			elseif d < 0 then
-				if IsShiftKeyDown() then
-					frame:ScrollToBottom()
-				elseif IsControlKeyDown() then
-					frame:PageDown()
-				--else -- Blizz function does this
-				--	frame:ScrollDown()
-				end
-			end
-		end
+	local clickFunc = function(frame)
+		frame:GetParent():ScrollToBottom()
+		frame:Hide()
+	end
+	local scrollFunc = function(frame)
 		local n = frame:GetName()
 		if frame:AtBottom() then
 			_G[n.."ButtonFrameBottomButton"]:Hide()
@@ -32,13 +17,31 @@ BCM.modules[#BCM.modules+1] = function()
 			_G[n.."ButtonFrameBottomButton"]:Show()
 		end
 	end
+	hooksecurefunc("FloatingChatFrame_OnMouseScroll", function(frame, d)
+		if d == 1 then
+			if IsShiftKeyDown() then
+				frame:ScrollToTop()
+			elseif IsControlKeyDown() then
+				frame:PageUp()
+			end
+		else
+			if IsShiftKeyDown() then
+				frame:ScrollToBottom()
+			elseif IsControlKeyDown() then
+				frame:PageDown()
+			end
+		end
+		scrollFunc(frame)
+	end)
 
-	local clickFunc = function(frame) frame:GetParent():ScrollToBottom() frame:Hide() end
+	-- Force our scroll hook to apply
+	InterfaceOptionsSocialPanelChatMouseScroll_SetScrolling("0")
+	InterfaceOptionsSocialPanelChatMouseScroll_SetScrolling("1")
+
 	BCM.chatFuncsPerFrame[#BCM.chatFuncsPerFrame+1] = function(n)
 		local btn = _G[n.."ButtonFrameBottomButton"]
 		btn:ClearAllPoints()
 		local cf = _G[n]
-		cf:HookScript("OnMouseWheel", scrollFunc)
 		cf:HookScript("OnShow", scrollFunc)
 		btn:SetParent(cf)
 		btn:SetPoint("TOPRIGHT")
