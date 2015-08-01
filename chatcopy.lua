022a9e50c6bf1a67b1d88740691b1a7be413a8ec
funkydude <funkydude@users.noreply.github.com>
2015-08-01 15:24:40 +0000

    Cleanup chatcopy code.

--- chatcopy.lua
+++ chatcopy.lua
@@ -5,6 +5,10 @@ local _, BCM = ...
 BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_ChatCopy then return end
 
+	local scrollDown = function()
+		BCMCopyScroll:SetVerticalScroll((BCMCopyScroll:GetVerticalScrollRange()) or 0)
+	end
+
 	--Copying Functions
 	local copyFunc = function(frame)
 		if not IsShiftKeyDown() then return end
@@ -16,9 +20,8 @@ BCM.modules[#BCM.modules+1] = function()
 		text = text:gsub("|[Tt]Interface\\TargetingFrame\\UI%-RaidTargetingIcon_(%d):0|[Tt]", "{rt%1}") -- I like being able to copy raid icons
 		text = text:gsub("|[Tt][^|]+|[Tt]", "") -- Remove any other icons to prevent copying issues
 		BCMCopyBox:SetText(text)
-		BCMCopyBox:HighlightText(0)
 		BCMCopyFrame:Show()
-		BCMCopyScroll.ScrollToBottom:Play() -- Scroll to the bottom, we have to delay it unfortunately
+		C_Timer.After(0.25, scrollDown) -- Scroll to the bottom, we have to delay it unfortunately
 	end
 	local hintFunc = function(frame)
 		if bcmDB.noChatCopyTip then return end
@@ -50,14 +53,6 @@ BCM.modules[#BCM.modules+1] = function()
 	local scrollArea = CreateFrame("ScrollFrame", "BCMCopyScroll", frame, "UIPanelScrollFrameTemplate")
 	scrollArea:SetPoint("TOPLEFT", frame, "TOPLEFT", 8, -5)
 	scrollArea:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -30, 5)
-
-	-- XXX temp till 6.0
-	local group = scrollArea:CreateAnimationGroup()
-	local update = group:CreateAnimation()
-	group:SetScript("OnFinished", function() BCMCopyScroll:SetVerticalScroll((BCMCopyScroll:GetVerticalScrollRange()) or 0) end)
-	update:SetOrder(1)
-	update:SetDuration(0.25)
-	BCMCopyScroll.ScrollToBottom = group
 
 	local editBox = CreateFrame("EditBox", "BCMCopyBox", frame)
 	editBox:SetMultiLine(true)
