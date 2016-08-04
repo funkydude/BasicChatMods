c907b374bb83d3bafd5000e3d829b1fca15118fa
funkydude <funkydude@users.noreply.github.com>
2016-08-04 14:22:30 +0000

    bnet: fix class colouring.

--- bnet.lua
+++ bnet.lua
@@ -12,21 +12,23 @@ BCM.modules[#BCM.modules+1] = function()
 		storedName = {}
 		local _, n = BNGetNumFriends()
 		for i=1, n do
-			local _, _, _, _, charName, id = BNGetFriendInfo(i)
-			storedName[id] = charName
+			local _, _, _, _, charName, bnetIDGameAccount = BNGetFriendInfo(i)
+			storedName[bnetIDGameAccount] = charName
 		end
 	end
 	local changeBNetName = function(icon, misc, id, moreMisc, fakeName, tag, colon)
-		local _, charName, _, _, _, _, _, englishClass = BNGetGameAccountInfo(id)
+		local _, _, _, _, _, bnetIDGameAccount = BNGetFriendInfoByID(id)
+		local _, charName, _, _, _, _, _, englishClass = BNGetGameAccountInfo(bnetIDGameAccount)
+
 		if charName ~= "" then
-			if storedName then storedName[id] = charName end --Store name for logoff events, if enabled
+			if storedName then storedName[bnetIDGameAccount] = charName end --Store name for logoff events, if enabled
 			--Replace real name with charname if enabled
 			fakeName = bcmDB.noRealName and charName or fakeName
 		else
 			--Replace real name with stored charname if enabled, for logoff events
-			if bcmDB.noRealName and storedName and storedName[id] then
-				fakeName = storedName[id]
-				storedName[id] = nil
+			if bcmDB.noRealName and storedName and storedName[bnetIDGameAccount] then
+				fakeName = storedName[bnetIDGameAccount]
+				storedName[bnetIDGameAccount] = nil
 			end
 		end
 		if englishClass and englishClass ~= "" then --Friend logging off/Starcraft 2
