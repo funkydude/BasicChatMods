55b796061e9940a9ee640bc892f7a57d8bd233ad
funkydude <funkydude@users.noreply.github.com>
2016-08-05 18:19:39 +0000

    BNet: Fix an error when a friend logs off. The "no real name" option now replaces real names with battle tags instead of wow character names.

--- bnet.lua
+++ bnet.lua
@@ -8,35 +8,25 @@ BCM.modules[#BCM.modules+1] = function()
 	if not bcmDB.playerLBrack then bcmDB.playerLBrack = "[" bcmDB.playerRBrack = "]" bcmDB.playerSeparator = ":" end
 
 	local storedName = nil
-	if bcmDB.noRealName then
-		storedName = {}
-		local _, n = BNGetNumFriends()
-		for i=1, n do
-			local _, _, _, _, charName, bnetIDGameAccount = BNGetFriendInfo(i)
-			storedName[bnetIDGameAccount] = charName
-		end
-	end
 	local changeBNetName = function(icon, misc, id, moreMisc, fakeName, tag, colon)
-		local _, _, _, _, _, bnetIDGameAccount = BNGetFriendInfoByID(id)
-		local _, charName, _, _, _, _, _, englishClass = BNGetGameAccountInfo(bnetIDGameAccount)
+		local _, _, battleTag, _, _, bnetIDGameAccount = BNGetFriendInfoByID(id)
+		local englishClass
+		if bnetIDGameAccount then
+			_, _, _, _, _, _, _, englishClass = BNGetGameAccountInfo(bnetIDGameAccount)
+		end
 
-		if charName ~= "" then
-			if storedName then storedName[bnetIDGameAccount] = charName end --Store name for logoff events, if enabled
-			--Replace real name with charname if enabled
-			fakeName = bcmDB.noRealName and charName or fakeName
-		else
-			--Replace real name with stored charname if enabled, for logoff events
-			if bcmDB.noRealName and storedName and storedName[bnetIDGameAccount] then
-				fakeName = storedName[bnetIDGameAccount]
-				storedName[bnetIDGameAccount] = nil
-			end
+		if bcmDB.noRealName then -- Replace real name with battle tag if enabled
+			fakeName = battleTag:gsub("^(.+)#%d+$", "%1")
 		end
-		if englishClass and englishClass ~= "" then --Friend logging off/Starcraft 2
-			fakeName = bcmDB.noBNetColor and fakeName or "|cFF"..BCM:GetColor(englishClass, true)..fakeName.."|r" --Color name if enabled
+
+		if englishClass and englishClass ~= "" then -- Not playing wow, logging off, etc.
+			fakeName = bcmDB.noBNetColor and fakeName or "|cFF"..BCM:GetColor(englishClass, true)..fakeName.."|r" -- Colour name if enabled
 		end
+
 		if bcmDB.noBNetIcon then --Remove "person" icon if enabled
 			icon = icon:gsub("|[Tt][^|]+|[Tt]", "")
 		end
+
 		return icon..misc..id..moreMisc..bcmDB.playerLBrack..fakeName..bcmDB.playerRBrack..tag..(colon == ":" and bcmDB.playerSeparator or colon)
 	end
 	BCM.chatFuncs[#BCM.chatFuncs+1] = function(text)
