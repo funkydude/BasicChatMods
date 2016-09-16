df71b08befd37b1e6a5270c3d02e46b7b1771ea9
funkydude <funkydude@users.noreply.github.com>
2016-09-16 09:23:32 +0100

    Move locale to the repo.

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -1,4 +1,56 @@
 
 if GetLocale() ~= "deDE" then return end
-local _, BCM = ...
---@localization(locale="deDE", namespace="Config_Module", format="lua_additive_table", table-name="BCM")@
+local _, L = ...
+L["BCM_AltInvite"] = "Macht alle Spielernamen im Chat ALT-Klickbar, was diesen Spieler in deine Gruppe einlädt."
+L["BCM_AutoLog"] = "Zeichnet den Chat automatisch nach dem Einloggen auf, ebenso den Kampflog während du in einer Schlachtzugsinstanz bist."
+L["BCM_BNet"] = "Verändert die Klammern oder fügt den verschiedenen Real-ID-Ereignissen, wie zum Beispiel Flüsternachrichten, Konversationen und einloggenden Freunden, Farbe hinzu. "
+L["BCM_ButtonHide"] = "Versteckt die seitlichen Buttons des Chatfenster komplett, dies ist für diejenigen, die keine Verwendung dafür haben."
+L["BCM_ChannelNames"] = "Ersetzt ausgewählte Channelnamen mit benutzerdefinierten Namen, die du magst. Z.B. [Gruppe] >> [G]"
+L["BCM_ChatCopy"] = "Dieses Modul erlaubt es Ihnen, durch |cFFFFFFFFSHIFT-Klick|r auf den Chatfenster-Tab, Chattext direkt zu kopieren."
+L["BCM_EditBox"] = "Verändert das Eingabefenster (Das Fenster, in das du tippst), wie zum Beispiel es nach oben zu bewegen, den Hintergrund zu verstecken oder die Größe zu vergrößern. "
+L["BCM_Fade"] = "Das Chatfenster wird komplett anstatt teilweise ausgeblendet, wenn du deine Maus von einem Chatfenster wegbewegst."
+L["BCM_Font"] = "Verändert die Schriftart, Größe und Aussehen deines Chat- & Eingabefensters."
+L["BCM_GMOTD"] = "Zeigt erneut die Gildennachricht des Tages im Hauptchatfenster nach 10 Sekunden an."
+L["BCM_Highlight"] = "Spielt einen Sound ab und färbt deinen Namen in Klassenfarbe ein, falls er im Chat erscheint. Du kannst auch eine Abkürzung deines Namens eingeben."
+L["BCM_History"] = "Wähle, wie viele Zeilen Chat dein Chatfenster behält."
+L["BCM_InviteLinks"] = "Sucht nach dem Wort 'invite' und wandelt dieses in einen ALT-Klickbaren Link, der diese Person einlädt, um. Z.B. |cFFFF7256[invite]|r"
+L["BCM_Justify"] = "Richtet den Text der verschiedenen Chatfenster nach links, rechts oder in die Mitte des Chatfensters aus."
+L["BCM_PlayerNames"] = "Fügt den Spielernamen im Chat die Spielerstufe/-gruppe (falls bekannt) hinzu oder entfernt/verändert Klammern. Z.B. [85:|cFFFFFFFFCoolerPriester|r:5]"
+L["BCM_Resize"] = "Erlaubt es dir, die Größe des Chatfensters größer oder kleiner zu machen als das, was Blizzard erlaubt."
+L["BCM_ScrollDown"] = "Erstellt einen kleinen anklickbaren Pfeil über deinen Chatfenstern, der blinkt, wenn du beim Text nicht ganz unten bist."
+L["BCM_Sticky"] = "Verändert deinen ''gemerkten' Chat. Dies bewirkt, dass das Chat-Eingabefenster sich den letzten Chattyp merkt, damit Du es nicht bei der nächsten Unterhaltung erneut eingeben musst."
+L["BCM_TellTarget"] = "Erlaubt es dir, dein aktuelles Ziel per /tt oder /wt anzuflüstern."
+L["BCM_Timestamp"] = "Passt den Zeitstempel, den der Chat benutzen soll, an. Wähle eine Farbe oder gar keine, wähle dann das genaue Format des Zeitstempels."
+L["BCM_URLCopy"] = "Verwandelt Internetadressen im Chat in anklickbare Links, sodass Du diese einfach kopieren kannst. Z.B |cFFFFFFFF[www.battle.net]|r"
+L["BNETICON"] = "Symbol %s entfernen."
+L["BOTTOM"] = "Unten"
+L["CENTER"] = "Mittig"
+L["CHANNELNAME"] = "Channelname"
+L["CHANNELNUMBER"] = "Channelnummer"
+L["CHATLOG"] = "Chat immer aufzeichnen."
+L["CLICKTOCOPY"] = "Shift-Klick, um den Chat zu kopieren."
+L["COLORMISC"] = "Freund-/Gilden-Einloggung in Klassenfarben."
+L["COMBATLOG"] = "Kampflog in Schlachtzugsinstanzen aufzeichnen."
+L["CORE"] = [=[Willkommen bei BasicChatMods, ein einfacher und modularer Ansatz zur Chat-Anpassung. Aufgrund der Art, wie BCM gestaltet ist, könnte ein /reload für manche Änderungen benötigt werden. 
+
+Standardmäßig wird dir BCM erlauben, deine Chatfenster an den äußersten Rand des Bildschirms zu ziehen, die verbleibende Anpassung wird in den Modulen von BCM gemacht, welche, je nach Wunsch, aktiviert oder deaktiviert werden können.
+
+In BCM deaktivierte Module verbrauchen keinen Speicher, deaktiviere die, die du nicht benutzt!]=]
+L["CUSTOMCHANNEL"] = "Benutzerdefinierter Channel"
+L["FAKENAMES"] = "Echte Namen mit Charakternamen ersetzen."
+L["GENERAL"] = "Allgemein"
+L["GUILDRECRUIT"] = "Gildenrekrutierung"
+L["LEFT"] = "Links"
+L["LFG"] = "SucheNachGruppe"
+L["LOCALDEFENSE"] = "LokaleVerteidigung"
+L["OPTIONS"] = "<<Mehr Optionen könnten durch das Einschalten dieses Modules verfügbar sein>>"
+L["PLAYERBRACKETS"] = "Spieler-Klammern:"
+L["RIGHT"] = "Rechts"
+L["SHOWGROUP"] = "Spielergruppe neben Namen."
+L["SHOWLEVELS"] = "Spielerstufe neben Namen."
+L["SIZE"] = "Größe"
+L["TOP"] = "Oben"
+L["TRADE"] = "Handel"
+L["WARNING"] = "<<Die Änderungen werden erst nach einem /reload wirksam>>"
+L["WORLDDEFENSE"] = "Weltverteidigung"
+
--- Locales/esMX.lua
+++
@@ -1,4 +0,0 @@
-
-if GetLocale() ~= "esMX" then return end
-local _, BCM = ...
---@localization(locale="esES", namespace="Config_Module", format="lua_additive_table", table-name="BCM")@
