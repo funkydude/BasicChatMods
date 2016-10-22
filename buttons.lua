8f8f0504e407fc0f0f25ece25d1fbe13b07cee6f
funkydude <funkydude@users.noreply.github.com>
2016-10-22 19:35:47 +0100

    Patch 7.1 fix for friends button hiding.

--- buttons.lua
+++ buttons.lua
@@ -8,8 +8,14 @@ BCM.modules[#BCM.modules+1] = function()
 	local hideFunc = function(frame) frame:Hide() end
 	ChatFrameMenuButton:HookScript("OnShow", hideFunc) --Hide the chat shortcut button for emotes/languages/etc
 	ChatFrameMenuButton:Hide() --Hide the chat shortcut button for emotes/languages/etc
-	FriendsMicroButton:HookScript("OnShow", hideFunc) --Hide the "Friends Online" count button
-	FriendsMicroButton:Hide() --Hide the "Friends Online" count button
+	if FriendsMicroButton then -- XXX v7
+		FriendsMicroButton:HookScript("OnShow", hideFunc) --Hide the "Friends Online" count button
+		FriendsMicroButton:Hide() --Hide the "Friends Online" count button
+	end
+	if QuickJoinToastButton then -- XXX v7.1
+		QuickJoinToastButton:HookScript("OnShow", hideFunc) --Hide the "Friends Online" count button
+		QuickJoinToastButton:Hide() --Hide the "Friends Online" count button
+	end
 
 	BCM.chatFuncsPerFrame[#BCM.chatFuncsPerFrame+1] = function(n)
 		local btnFrame = _G[n.."ButtonFrame"]
