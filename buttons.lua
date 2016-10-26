1c8c265e1c91bd9a57c9c2b99fd212740b2f021d
funkydude <funkydude@users.noreply.github.com>
2016-10-26 12:32:30 +0100

    buttons: remove compat

--- buttons.lua
+++ buttons.lua
@@ -8,11 +8,7 @@ BCM.modules[#BCM.modules+1] = function()
 	local hideFunc = function(frame) frame:Hide() end
 	ChatFrameMenuButton:HookScript("OnShow", hideFunc) --Hide the chat shortcut button for emotes/languages/etc
 	ChatFrameMenuButton:Hide() --Hide the chat shortcut button for emotes/languages/etc
-	if FriendsMicroButton then -- XXX v7
-		FriendsMicroButton:HookScript("OnShow", hideFunc) --Hide the "Friends Online" count button
-		FriendsMicroButton:Hide() --Hide the "Friends Online" count button
-	end
-	if QuickJoinToastButton then -- XXX v7.1
+	if QuickJoinToastButton then
 		QuickJoinToastButton:HookScript("OnShow", hideFunc) --Hide the "Friends Online" count button
 		QuickJoinToastButton:Hide() --Hide the "Friends Online" count button
 	end
