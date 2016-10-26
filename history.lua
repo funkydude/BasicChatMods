3ec829258f49c317a6cc4dbd49d23fc8df20cd51
funkydude <funkydude@users.noreply.github.com>
2016-10-26 12:00:39 +0100

    history: SetMaxLines no longer clears the chat frame, remove giant block of code.

--- history.lua
+++ history.lua
@@ -6,38 +6,12 @@ BCM.earlyModules[#BCM.earlyModules+1] = function()
 	if bcmDB.BCM_History then bcmDB.lines, bcmDB.savedChat = nil, nil end
 
 	if not bcmDB.lines then bcmDB.lines = {["ChatFrame1"] = 1000} end
-	for k, v in pairs(bcmDB.lines) do
-		local f, tbl = _G[k], {}
-
-		for i = 1, f:GetNumMessages() do
-			local text, accessID, lineID, extraData = f:GetMessageInfo(i)
-			local cType = ChatHistory_GetChatType(extraData)
-			local info = ChatTypeInfo[cType]
-			if not info then -- Message was probably printed by an addon
-				for i = select("#", f:GetRegions()), 1, -1 do
-					local region = select(i, f:GetRegions())
-					if region:GetObjectType() == "FontString" and text == region:GetText() then
-						local r, g, b = region:GetTextColor()
-						tbl[#tbl+1] = {text, r, g, b, lineID, false, accessID, extraData}
-						info = true
-						break
-					end
-				end
-				if not info then -- Don't think this is needed, but better safe than sorry. If we didn't find the message, print it white.
-					tbl[#tbl+1] = {text, 1, 1, 1, lineID, false, accessID, extraData}
-				end
-			else -- Normal chat message
-				tbl[#tbl+1] = {text, info.r, info.g, info.b, lineID, false, accessID, extraData}
-			end
-		end
-		f:SetMaxLines(v) -- Set the max lines, which unfortunately clears the chat frame, resulting in all this extra chat restoration code :'(
+	for k, v in next, bcmDB.lines do
+		local f = _G[k]
+		f:SetMaxLines(v)
 		if k == "ChatFrame2" then
 			COMBATLOG_MESSAGE_LIMIT = v -- Blizzard keeps changing the combat log max lines in Blizzard_CombatLog_Refilter... this better not taint.
 		end
-		for i = 1, #tbl do
-			f:AddMessage(unpack(tbl[i]))
-		end
-		tbl=nil
 	end
 
 	if true and bcmDB.savedChat then
