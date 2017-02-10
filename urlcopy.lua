f681de48b7ce50d86ff35e9666b12e7e85f062ca
funkydude <funkydude@users.noreply.github.com>
2017-02-10 02:19:24 +0000

    urlcopy: Cleanup the pattern matching code to lower the false positive rate, especially on non-english languages. Please report any regressions (missed URLs) from this change.

--- urlcopy.lua
+++ urlcopy.lua
@@ -6,33 +6,49 @@ BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_URLCopy then return end
 
 	-- This functionality used to be stricter and more complex, but with the introduction
-	-- of gTLDs of any form or language, we can now simplify it down to a few patterns.
+	-- of gTLDs of any form or language, we can now reduce the amount of patterns.
+
 	-- Sentences like "I wish.For someone" will become URLs now "[wish.For]", and although they seem
 	-- like false positives, they can be genuine.
+
+	-- Easily readable, right? :D
+	-- We're converting anything in the form of "word.word" to a URL,
+	-- but we're adding a list of excluded symbols such as {}[]` that aren't a valid address, to prevent possible false positives.
+	-- Also note that the only difference between the 1st and 2nd section of the pattern is that the 2nd has a few extra
+	-- valid (but invalid in their location) things ".", "/", "," to prevent words like "lol...", "true./" and "yes.," becoming a URL.
+	-- As of the introduction of the S.E.L.F.I.E camera we now require at least 2 valid letters e.g. yo.hi
 	local filterFunc = function(_, _, msg, ...)
 		local newMsg, found = gsub(msg,
-			-- Easily readable, right? :D
-			-- We're converting anything in the form of "word.word" to a URL,
-			-- but we're adding a list of excluded symbols such as {}[]` that aren't a valid
-			-- address, to prevent possible false positives.
-			-- Also note that the only difference between the 1st and 2nd section of the pattern is that the 2nd has a few extra
-			-- valid (but invalid in their location) things ".", "/", "," to prevent words like "lol...", "true./" and "yes.," becoming a URL.
-			-- As of the introduction of the S.E.L.F.I.E camera we now require at least 2 valid letters e.g. yo.hi
-			"[^ \"£%^`¬{}%[%]\\|<>]*[^ '%-=%./,\"£%^`¬{}%[%]\\|<>%d][^ '%-=%./,\"£%^`¬{}%[%]\\|<>%d]%.[^ '%-=%./,\"£%^`¬{}%[%]\\|<>%d][^ '%-=%./,\"£%^`¬{}%[%]\\|<>%d][^ \"£%^`¬{}%[%]\\|<>]*",
+			" [^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]*%.?[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]+[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]%.[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d][^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]+/[^ \"%^`{}%[%]\\|<>]+",
+			"|cffffffff|Hbcmurl~%1|h[%1]|h|r"
+		)
+		newMsg, found = gsub(msg,
+			"^[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]*%.?[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]+[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]%.[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d][^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]+/[^ \"%^`{}%[%]\\|<>]+",
+			"|cffffffff|Hbcmurl~%1|h[%1]|h|r"
+		)
+		if found > 0 then return false, newMsg, ... end
+		newMsg, found = gsub(msg,
+			" [^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]*%.?[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]+[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]%.[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d][^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]+",
+			"|cffffffff|Hbcmurl~%1|h[%1]|h|r"
+		)
+		if found > 0 then return false, newMsg, ... end
+		newMsg, found = gsub(msg,
+			"^[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]*%.?[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]+[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]%.[^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d][^ %%'%-=%./,\"%^`{}%[%]\\|<>%(%)%d]+",
 			"|cffffffff|Hbcmurl~%1|h[%1]|h|r"
 		)
 		if found > 0 then return false, newMsg, ... end
 		newMsg, found = gsub(msg,
 			-- Numbers are banned from the first pattern to prevent false positives like "5.5k" etc.
 			-- This is our IPv4/v6 pattern at the beggining of a sentence.
-			"^%x+[%.:]%x+[%.:]%x+[%.:]%x+[^ \"£%^`¬{}%[%]\\|<>]*",
+			"^%x+[%.:]%x+[%.:]%x+[%.:]%x+[^ \"%^`{}%[%]\\|<>]*",
 			"|cffffffff|Hbcmurl~%1|h[%1]|h|r"
 		)
+		
 		if found > 0 then return false, newMsg, ... end
 		newMsg, found = gsub(msg,
 			-- This is our mid-sentence IPv4/v6 pattern, we separate the IP patterns into 2 to prevent
 			-- false positives with linking items, spells, etc.
-			" %x+[%.:]%x+[%.:]%x+[%.:]%x+[^ \"£%^`¬{}%[%]\\|<>]*",
+			" %x+[%.:]%x+[%.:]%x+[%.:]%x+[^ \"%^`{}%[%]\\|<>]*",
 			"|cffffffff|Hbcmurl~%1|h[%1]|h|r"
 		)
 		if found > 0 then return false, newMsg, ... end
