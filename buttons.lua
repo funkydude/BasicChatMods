f759e981558148f8e3320487eb60fa3bc23439c0
funkydude <funkydude@users.noreply.github.com>
2018-07-18 20:24:57 +0100

    buttons: Add the new BfA buttons to the hider.

--- buttons.lua
+++ buttons.lua
@@ -6,8 +6,14 @@ BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_ButtonHide then return end
 
 	local hideFunc = function(frame) frame:Hide() end
-	ChatFrameMenuButton:HookScript("OnShow", hideFunc) --Hide the chat shortcut button for emotes/languages/etc
+	ChatFrameMenuButton:HookScript("OnShow", hideFunc)
 	ChatFrameMenuButton:Hide() --Hide the chat shortcut button for emotes/languages/etc
+	ChatFrameToggleVoiceDeafenButton:HookScript("OnShow", hideFunc)
+	ChatFrameToggleVoiceDeafenButton:Hide() --Hide the voice deafen button
+	ChatFrameToggleVoiceMuteButton:HookScript("OnShow", hideFunc)
+	ChatFrameToggleVoiceMuteButton:Hide() --Hide the voice mute button
+	ChatFrameChannelButton:HookScript("OnShow", hideFunc)
+	ChatFrameChannelButton:Hide() --Hide the voice mute button
 	if QuickJoinToastButton then
 		QuickJoinToastButton:HookScript("OnShow", hideFunc) --Hide the "Friends Online" count button
 		QuickJoinToastButton:Hide() --Hide the "Friends Online" count button
