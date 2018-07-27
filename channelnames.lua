44fbbc568b52566d71c1b3e9be57aa68676d85a4
funkydude <funkydude@users.noreply.github.com>
2018-07-27 12:21:50 +0100

    channelnames: Replacements for channel names now work with more than 10 custom channels.

--- channelnames.lua
+++ channelnames.lua
@@ -30,12 +30,12 @@ BCM.modules[#BCM.modules+1] = function()
 	local rplc = bcmDB.replacements
 	local gsub = gsub
 	local chn = {
-		"%[%d0?%. General[^%]]*%]",
-		"%[%d0?%. Trade[^%]]*%]",
-		"%[%d0?%. WorldDefense[^%]]*%]",
-		"%[%d0?%. LocalDefense[^%]]*%]",
-		"%[%d0?%. LookingForGroup[^%]]*%]",
-		"%[%d0?%. GuildRecruitment[^%]]*%]",
+		"%[%d%d?%. General[^%]]*%]",
+		"%[%d%d?%. Trade[^%]]*%]",
+		"%[%d%d?%. WorldDefense[^%]]*%]",
+		"%[%d%d?%. LocalDefense[^%]]*%]",
+		"%[%d%d?%. LookingForGroup[^%]]*%]",
+		"%[%d%d?%. GuildRecruitment[^%]]*%]",
 		gsub(CHAT_INSTANCE_CHAT_GET, ".*%[(.*)%].*", "%%[%1%%]"),
 		gsub(CHAT_INSTANCE_CHAT_LEADER_GET, ".*%[(.*)%].*", "%%[%1%%]"),
 		gsub(CHAT_GUILD_GET, ".*%[(.*)%].*", "%%[%1%%]"),
@@ -46,45 +46,45 @@ BCM.modules[#BCM.modules+1] = function()
 		gsub(CHAT_RAID_GET, ".*%[(.*)%].*", "%%[%1%%]"),
 		gsub(CHAT_RAID_LEADER_GET, ".*%[(.*)%].*", "%%[%1%%]"),
 		gsub(CHAT_RAID_WARNING_GET, ".*%[(.*)%].*", "%%[%1%%]"),
-		"%[(%d0?)%. ([^ ]-)%]", --Custom Channels
+		"%[(%d%d?)%. ([^%]]+)%]", --Custom Channels
 	}
 
 	local L = GetLocale()
 	if L == "ruRU" then --Russian
-		chn[1] = "%[%d0?%. Общий.-%]"
-		chn[2] = "%[%d0?%. Торговля.-%]"
-		chn[3] = "%[%d0?%. Оборона: глобальный%]" --Defense: Global
-		chn[4] = "%[%d0?%. Оборона.-%]" --Defense: Zone
-		chn[5] = "%[%d0?%. Поиск спутников%]"
-		chn[6] = "%[%d0?%. Гильдии.-%]"
+		chn[1] = "%[%d%d?%. Общий.-%]"
+		chn[2] = "%[%d%d?%. Торговля.-%]"
+		chn[3] = "%[%d%d?%. Оборона: глобальный%]" --Defense: Global
+		chn[4] = "%[%d%d?%. Оборона.-%]" --Defense: Zone
+		chn[5] = "%[%d%d?%. Поиск спутников%]"
+		chn[6] = "%[%d%d?%. Гильдии.-%]"
 	elseif L == "deDE" then --German
-		chn[1] = "%[%d0?%. Allgemein[^%]]*%]"
-		chn[2] = "%[%d0?%. Handel[^%]]*%]"
-		chn[3] = "%[%d0?%. Weltverteidigung[^%]]*%]"
-		chn[4] = "%[%d0?%. LokaleVerteidigung[^%]]*%]"
-		chn[5] = "%[%d0?%. SucheNachGruppe[^%]]*%]"
-		chn[6] = "%[%d0?%. Gildenrekrutierung[^%]]*%]"
+		chn[1] = "%[%d%d?%. Allgemein[^%]]*%]"
+		chn[2] = "%[%d%d?%. Handel[^%]]*%]"
+		chn[3] = "%[%d%d?%. Weltverteidigung[^%]]*%]"
+		chn[4] = "%[%d%d?%. LokaleVerteidigung[^%]]*%]"
+		chn[5] = "%[%d%d?%. SucheNachGruppe[^%]]*%]"
+		chn[6] = "%[%d%d?%. Gildenrekrutierung[^%]]*%]"
 	elseif L == "frFR" then --French
-		chn[1] = "%[%d0?%. Général[^%]]*%]"
-		chn[2] = "%[%d0?%. Commerce[^%]]*%]"
-		chn[3] = "%[%d0?%. DéfenseUniverselle[^%]]*%]"
-		chn[4] = "%[%d0?%. DéfenseLocale[^%]]*%]"
-		chn[5] = "%[%d0?%. RechercheDeGroupe[^%]]*%]"
-		chn[6] = "%[%d0?%. RecrutementDeGuilde[^%]]*%]"
+		chn[1] = "%[%d%d?%. Général[^%]]*%]"
+		chn[2] = "%[%d%d?%. Commerce[^%]]*%]"
+		chn[3] = "%[%d%d?%. DéfenseUniverselle[^%]]*%]"
+		chn[4] = "%[%d%d?%. DéfenseLocale[^%]]*%]"
+		chn[5] = "%[%d%d?%. RechercheDeGroupe[^%]]*%]"
+		chn[6] = "%[%d%d?%. RecrutementDeGuilde[^%]]*%]"
 	elseif L == "zhTW" then --Traditional Chinese
-		chn[1] = "%[%d0?%. 綜合.-%]"
-		chn[2] = "%[%d0?%. 交易.-%]"
-		chn[3] = "%[%d0?%. 世界防務%]"
-		chn[4] = "%[%d0?%. 本地防務.-%]"
-		chn[5] = "%[%d0?%. 尋求組隊%]"
-		chn[6] = "%[%d0?%. 公會招募.-%]"
+		chn[1] = "%[%d%d?%. 綜合.-%]"
+		chn[2] = "%[%d%d?%. 交易.-%]"
+		chn[3] = "%[%d%d?%. 世界防務%]"
+		chn[4] = "%[%d%d?%. 本地防務.-%]"
+		chn[5] = "%[%d%d?%. 尋求組隊%]"
+		chn[6] = "%[%d%d?%. 公會招募.-%]"
 	elseif L == "koKR" then --Korean
-		chn[1] = "%[%d0?%. 일반.-%]"
-		chn[2] = "%[%d0?%. 거래.-%]"
-		chn[3] = "%[%d0?%. 광역수비%]"
-		chn[4] = "%[%d0?%. 지역수비.-%]"
-		chn[5] = "%[%d0?%. 파티찾기%]"
-		chn[6] = "%[%d0?%. 길드찾기.-%]"
+		chn[1] = "%[%d%d?%. 일반.-%]"
+		chn[2] = "%[%d%d?%. 거래.-%]"
+		chn[3] = "%[%d%d?%. 광역수비%]"
+		chn[4] = "%[%d%d?%. 지역수비.-%]"
+		chn[5] = "%[%d%d?%. 파티찾기%]"
+		chn[6] = "%[%d%d?%. 길드찾기.-%]"
 	end
 
 	local num = #chn
