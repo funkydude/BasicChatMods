6a477ef38976cecef0b77ba479590e0f5a01bedf
funkydude <funkydude@users.noreply.github.com>
2019-06-20 01:01:34 +0100

    highlight: Fix sound notification in patch 8.2

--- highlight.lua
+++ highlight.lua
@@ -64,7 +64,7 @@ BCM.modules[#BCM.modules+1] = function()
 			for i=1, #myNames do
 				local newMsg, found = gsub(msg, myNames[i], color)
 				if found > 0 then
-					PlaySoundFile("Sound\\interface\\iTellMessage.ogg", "Master")
+					PlaySound(3081, "Master", false) -- SOUNDKIT.TELL_MESSAGE
 					return false, newMsg, player, ...
 				end
 			end
