3fd460965241e4632d4271feeea5e7a57c7fecb0
funkydude <funkydude@users.noreply.github.com>
2019-10-08 17:28:58 +0100

    AltInvite: Update API usage.

--- AltInvite.lua
+++ AltInvite.lua
@@ -4,11 +4,12 @@
 local _, BCM = ...
 BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_AltInvite then return end
+	local InviteUnit = C_PartyInfo.InviteUnit
 
 	hooksecurefunc("SetItemRef", function(link)
 		if IsAltKeyDown() then
 			local player = link:match("^player:([^:]+)")
-			InviteToGroup(player)
+			InviteUnit(player)
 			-- We use a secure hook to stay clean (avoid taint), but this means a whisper window will open, so we close it.
 			ChatEdit_OnEscapePressed(ChatFrame1EditBox)
 		end
