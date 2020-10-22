3f964a159839dd53f426215194ac57a0356c593b
funkydude <funkydude@users.noreply.github.com>
2020-10-22 02:09:18 +0100

    AltInvite: Fix alt-clicking BNet chat not inviting players to group, closes #34

--- AltInvite.lua
+++ AltInvite.lua
@@ -8,10 +8,24 @@ BCM.modules[#BCM.modules+1] = function()
 
 	hooksecurefunc("SetItemRef", function(link)
 		if IsAltKeyDown() then
+			-- Normal chat: player:<name>:<lineId>:WHISPER:<name>
+			-- BNet chat: BNplayer:<bNetName>:<bNetGameAccountID>:<lineId>:BN_WHISPER:<bNetName>
 			local player = link:match("^player:([^:]+)")
-			InviteUnit(player)
-			-- We use a secure hook to stay clean (avoid taint), but this means a whisper window will open, so we close it.
-			ChatEdit_OnEscapePressed(ChatFrame1EditBox)
+			if player then
+				C_PartyInfo.InviteUnit(player)
+				-- We use a secure hook to stay clean (avoid taint), but this means a whisper window will open, so we close it.
+				ChatEdit_OnEscapePressed(ChatFrame1EditBox)
+			else
+				local gameAccountID = link:match("^BNplayer:[^:]+:([^:]+)")
+				if gameAccountID then
+					local accountInfoTbl = C_BattleNet.GetAccountInfoByID(gameAccountID)
+					if accountInfoTbl and accountInfoTbl.gameAccountInfo and accountInfoTbl.gameAccountInfo.gameAccountID then
+						BNInviteFriend(accountInfoTbl.gameAccountInfo.gameAccountID)
+						-- We use a secure hook to stay clean (avoid taint), but this means a whisper window will open, so we close it.
+						ChatEdit_OnEscapePressed(ChatFrame1EditBox)
+					end
+				end
+			end
 		end
 	end)
 end
