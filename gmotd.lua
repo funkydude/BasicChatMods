54e57c2af0413015792af8c74ed1ae181d81f89a
funkydude <funkydude@users.noreply.github.com>
2021-04-01 17:03:47 +0100

    gmotd: Cleanup

--- gmotd.lua
+++ gmotd.lua
@@ -5,32 +5,18 @@ local _, BCM = ...
 BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_GMOTD or not IsInGuild() then return end
 
-	BCM.Events.t = GetTime()
-	BCM.Events.count = 1
-	BCM.Events:SetScript("OnUpdate", function(frame)
-		if (GetTime() - frame.t) > 10 then
-			local gmotd = GetGuildRosterMOTD()
-			--Sometimes the GMOTD isn't available at login/reload due to lag.
-			--We delay printing until it is available, but we can't assume that
-			--no GMOTD means it isn't available, it might mean no GMOTD was set.
-			--If it's still blank after 5 attempts, there probably isn't one set.
-			if gmotd == "" or gmotd:find("^ +$") then
-				if frame.count > 4 then
-					frame.t = nil
-					frame.count = nil
-					frame:SetScript("OnUpdate", nil)
-				else
-					frame.t = GetTime() - 7
-					frame.count = frame.count + 1
-				end
-			else
+	BCM.Events.LOADING_SCREEN_DISABLED = function()
+		BCM.Events:UnregisterEvent("LOADING_SCREEN_DISABLED")
+		BCM.Events.LOADING_SCREEN_DISABLED = nil
+		C_Timer.After(0, function() -- Timers become functional 1 frame after the loading screen is done
+			C_Timer.After(2, function()
+				local gmotd = GetGuildRosterMOTD()
+				if gmotd == "" or gmotd:find("^ +$") then return end
 				local info = ChatTypeInfo.GUILD
 				ChatFrame1:AddMessage("|cFF33FF99BasicChatMods|r: "..(GUILD_MOTD_TEMPLATE):format(gmotd), info.r, info.g, info.b)
-				frame.t = nil
-				frame.count = nil
-				frame:SetScript("OnUpdate", nil)
-			end
-		end
-	end)
+			end)
+		end)
+	end
+	BCM.Events:RegisterEvent("LOADING_SCREEN_DISABLED")
 end
 
