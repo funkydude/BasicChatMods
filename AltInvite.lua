8162679de75ee9b0a90bdc90d356df9f35df9e6e
funkydude <funkydude@users.noreply.github.com>
2021-06-01 02:46:44 +0100

    AltInvite: Tweak

--- AltInvite.lua
+++ AltInvite.lua
@@ -5,6 +5,8 @@ local _, BCM = ...
 BCM.modules[#BCM.modules+1] = function()
 	if bcmDB.BCM_AltInvite then return end
 	local InviteUnit = C_PartyInfo.InviteUnit
+	local GetAccountInfoByID = C_BattleNet.GetAccountInfoByID
+	local BNInviteFriend = BNInviteFriend
 
 	hooksecurefunc("SetItemRef", function(link)
 		if IsAltKeyDown() then
@@ -12,13 +14,13 @@ BCM.modules[#BCM.modules+1] = function()
 			-- BNet chat: BNplayer:<bNetName>:<bNetGameAccountID>:<lineId>:BN_WHISPER:<bNetName>
 			local player = link:match("^player:([^:]+)")
 			if player then
-				C_PartyInfo.InviteUnit(player)
+				InviteUnit(player)
 				-- We use a secure hook to stay clean (avoid taint), but this means a whisper window will open, so we close it.
 				ChatEdit_OnEscapePressed(ChatFrame1EditBox)
 			else
-				local gameAccountID = link:match("^BNplayer:[^:]+:([^:]+)")
-				if gameAccountID then
-					local accountInfoTbl = C_BattleNet.GetAccountInfoByID(gameAccountID)
+				local bnetAccountID = link:match("^BNplayer:[^:]+:([^:]+)")
+				if bnetAccountID then
+					local accountInfoTbl = GetAccountInfoByID(bnetAccountID)
 					if accountInfoTbl and accountInfoTbl.gameAccountInfo and accountInfoTbl.gameAccountInfo.gameAccountID then
 						BNInviteFriend(accountInfoTbl.gameAccountInfo.gameAccountID)
 						-- We use a secure hook to stay clean (avoid taint), but this means a whisper window will open, so we close it.
