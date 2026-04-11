313cde9a0e15d243878a0fec7fa77058767ace9f
funkydude <funkydude@users.noreply.github.com>
2026-04-11 01:37:36 +0100

    gmotd: Add a safety check in case we've entered messaging lockdown

--- gmotd.lua
+++ gmotd.lua
@@ -10,10 +10,12 @@ BCM.modules[#BCM.modules+1] = function()
 		BCM.Events.LOADING_SCREEN_DISABLED = nil
 		C_Timer.After(0, function() -- Timers become functional 1 frame after the loading screen is done
 			C_Timer.After(2, function()
-				local gmotd = GetGuildRosterMOTD()
-				if gmotd == "" or gmotd:find("^ +$") then return end
-				local info = ChatTypeInfo.GUILD
-				ChatFrame1:AddMessage("|cFF33FF99BasicChatMods|r: "..(GUILD_MOTD_TEMPLATE):format(gmotd), info.r, info.g, info.b)
+				if not C_ChatInfo.InChatMessagingLockdown or not C_ChatInfo.InChatMessagingLockdown() then
+					local gmotd = GetGuildRosterMOTD()
+					if not gmotd or gmotd == "" or gmotd:find("^ +$") then return end
+					local info = ChatTypeInfo.GUILD
+					ChatFrame1:AddMessage("|cFF33FF99BasicChatMods|r: "..(GUILD_MOTD_TEMPLATE):format(gmotd), info.r, info.g, info.b)
+				end
 			end)
 		end)
 	end
