8980a34d1c1f783521e205209145bf94a4ba194a
funkydude <funkydude@users.noreply.github.com>
2026-04-11 15:52:26 +0100

    gmotd: Just grab the motd before any protections take place

--- gmotd.lua
+++ gmotd.lua
@@ -2,23 +2,28 @@
 --[[     GMOTD Module     ]]--
 
 local _, BCM = ...
-BCM.modules[#BCM.modules+1] = function()
-	if bcmDB.BCM_GMOTD or not IsInGuild() then return end
+BCM.earlyModules[#BCM.earlyModules+1] = function()
+	if bcmDB.BCM_GMOTD then return end
 
+	local gmotd = C_GuildInfo and C_GuildInfo.GetMOTD and C_GuildInfo.GetMOTD() or GetGuildRosterMOTD()
 	BCM.Events.LOADING_SCREEN_DISABLED = function()
 		BCM.Events:UnregisterEvent("LOADING_SCREEN_DISABLED")
 		BCM.Events.LOADING_SCREEN_DISABLED = nil
+		BCM.Events:UnregisterEvent("GUILD_MOTD")
+		BCM.Events.GUILD_MOTD = nil
+
 		C_Timer.After(0, function() -- Timers become functional 1 frame after the loading screen is done
 			C_Timer.After(2, function()
-				if not C_ChatInfo.InChatMessagingLockdown or not C_ChatInfo.InChatMessagingLockdown() then
-					local gmotd = GetGuildRosterMOTD()
-					if not gmotd or gmotd == "" or gmotd:find("^ +$") then return end
-					local info = ChatTypeInfo.GUILD
-					ChatFrame1:AddMessage("|cFF33FF99BasicChatMods|r: "..(GUILD_MOTD_TEMPLATE):format(gmotd), info.r, info.g, info.b)
-				end
+				if not gmotd or gmotd == "" or gmotd:find("^ +$") then return end
+				local info = ChatTypeInfo.GUILD
+				ChatFrame1:AddMessage("|cFF33FF99BasicChatMods|r: "..(GUILD_MOTD_TEMPLATE):format(gmotd), info.r, info.g, info.b)
 			end)
 		end)
 	end
 	BCM.Events:RegisterEvent("LOADING_SCREEN_DISABLED")
+	BCM.Events.GUILD_MOTD = function(_, msg)
+		gmotd = msg
+	end
+	BCM.Events:RegisterEvent("GUILD_MOTD")
 end
 
